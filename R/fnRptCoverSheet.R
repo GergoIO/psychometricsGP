2545edb02344124593c3024645e4e57431ac2ec0
Gergo <gergopinter@live.co.uk>
2022-06-16 09:43:11 +0100

    Coversheet function updated with new variable names

--- R/fnRptCoverSheet.R
+++ R/fnRptCoverSheet.R
@@ -4,10 +4,10 @@
 #' @param rptTitle This string will be the report title, shown prominently on the coverpage
 #' @param lstOfDetails This variable should be a list containing as least the following defined variables: academicYear, programme, stage, module, assessmentNumber, assessmentDate. These variables will be input on the cover page.
 #'
-#' @return Nothin is explicitly returned, rather the cover page of the variable containing the report is populated with the required information.
+#' @return Nothing is explicitly returned, rather the cover page of the variable containing the report is populated with the required information.
 #' @export
 #'
-#' @examples fnRptCoverSheet(rptName, rptType, lstOfDetails)
+#' @examples fnRptCoverSheet(rptVar, rptTitle, lstOfDetails)
 
 ################################################################################
 
@@ -16,66 +16,66 @@ fnRptCoverSheet <-
             rptTitle = NULL,
             lstOfDetails = NULL) {
     continue <- TRUE
-    if (is.null(rptName) == TRUE |
-        is.null(rptType) == TRUE |
+    if (is.null(rptVar) == TRUE |
+        is.null(rptTitle) == TRUE |
         is.null(lstOfDetails) == TRUE) {
       continue <- FALSE
       warning("One of the required variables for this function has not been specified.")
     }
     if (continue == TRUE) {
-      rptName <-
+      rptVar <-
         body_replace_all_text(
-          rptName,
+          rptVar,
           old_value = "YearPlaceholder",
           new_value = lstOfDetails$academicYear,
           only_at_cursor = FALSE
         )
-      rptName <-
+      rptVar <-
         body_replace_all_text(
-          rptName,
+          rptVar,
           old_value = "ProgrammePlaceholder",
           new_value = lstOfDetails$programme,
           only_at_cursor = FALSE
         )
-      rptName <-
+      rptVar <-
         body_replace_all_text(
-          rptName,
+          rptVar,
           old_value = "StagePlaceholder",
           new_value = as.character(lstOfDetails$stage),
           only_at_cursor = FALSE
         )
-      rptName <-
+      rptVar <-
         body_replace_all_text(
-          rptName,
+          rptVar,
           old_value = "ModulePlaceholder",
           new_value = lstOfDetails$module,
           only_at_cursor = FALSE
         )
-      rptName <-
+      rptVar <-
         body_replace_all_text(
-          rptName,
+          rptVar,
           old_value = "AssessmentPlaceholder",
           new_value = lstOfDetails$assessmentNumber,
           only_at_cursor = FALSE
         )
-      rptName <-
+      rptVar <-
         body_replace_all_text(
-          rptName,
+          rptVar,
           old_value = "DatePlaceholder",
           new_value = lstOfDetails$assessmentDate,
           only_at_cursor = FALSE
         )
-      rptName <-
-        cursor_reach(rptName, keyword = "ReportTitlePlaceholder")
-      rptName <-
-        body_add_par(rptName, rptType, style = "heading 1", pos = "on")
-      rptName <-
+      rptVar <-
+        cursor_reach(rptVar, keyword = "ReportTitlePlaceholder")
+      rptVar <-
+        body_add_par(rptVar, rptTitle, style = "heading 1", pos = "on")
+      rptVar <-
         body_replace_all_text(
-          rptName,
+          rptVar,
           old_value = "StartPlaceholder",
           new_value = "",
           only_at_cursor = FALSE
         )
-      rptName <- cursor_end(rptName)
+      rptVar <- cursor_end(rptVar)
     }
   }# END
