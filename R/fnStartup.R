9a3b0313dcc4d8d62d3e7a4123afcfe3268a9a85
Gergo <gergopinter@live.co.uk>
2022-06-20 15:13:12 +0100

    Added variables and processes for specific control of which folders to generate

--- R/fnStartup.R
+++ R/fnStartup.R
@@ -2,7 +2,9 @@
 #'
 #' @param githubToken The authorization token needed to access the relevant github repos
 #' @param verNum Set a version number - appended to save folders and file names
+#' @param createOutputFolder (TRUE/FALSE/NULL) Create an outputs folder in the working directory (with appended version number) if TRUE
 #' @param createPulseFolder (TRUE/FALSE/NULL) Create a folder called Pulse in the Output folder if TRUE
+#' @param createImagesFolder (TRUE/FALSE/NULL) Create an Images folder in the working directory (with appended version number) if TRUE
 #' #'
 #' @return Required packages are installed and loaded if they are not already.
 #' The working directory is set to the location of the script file and save as variable folderWD.
@@ -15,13 +17,22 @@
 
 fnStartup <- function(githubToken = NULL,
                       verNum = NULL,
-                      createPulseFolder = NULL) {
+                      createOutputFolder = NULL,
+                      createPulseSubfolder = NULL,
+                      createImagesFolder = NULL) {
+
   #   ____________________________________________________________________________
   #   Install Packages                                                        ####
 
   if (is.null(githubToken) == TRUE | is.null(verNum) == TRUE) {
     stop(
       "Please specify the github personal access token for file retrieval and a version number for file naming."
+    )
+  } else if (is.null(createOutputFolder) == TRUE |
+             is.null(createImagesFolder) == TRUE |
+             is.null(createPulseSubfolder) == TRUE) {
+    warning(
+      "Please specify the booleans createOutputFolder, createImagesFolder and createPulseSubfolder to determine which folders should be created. No folders will be created for any underfined booleans."
     )
   } else {
     # Install psychometricsTNG packages from private Github
@@ -76,20 +87,33 @@ fnStartup <- function(githubToken = NULL,
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
   folderWD <<- getwd()
 
-  # Create folders, if they don't exist https://stackoverflow.com/questions/40714190/use-apply-functions-dir-exist-and-dir-create
-  lapply(c(glue('Output ({verNum})'), glue('Figures ({verNum})')), function(x)
-    if (!dir.exists(x))
-      dir.create(
-        x,
-        showWarnings = TRUE,
-        recursive = FALSE,
-        mode = "0777"
-      ))
+  # Create folders, if they don't exist
+  # https://stackoverflow.com/questions/40714190/use-apply-functions-dir-exist-and-dir-create
+  if (createOutputFolder != FALSE) {
+    lapply(c(glue('Output ({verNum})')), function(x)
+      if (!dir.exists(x))
+        dir.create(
+          x,
+          showWarnings = TRUE,
+          recursive = FALSE,
+          mode = "0777"
+        ))
+    folderOutput <<- file.path(glue('{getwd()}\\Output ({verNum})'))
+  }
 
-  folderOutput <<- file.path(glue('{getwd()}\\Output ({verNum})'))
-  folderPlots <<- file.path(glue('{getwd()}\\Figures ({verNum})'))
+  if (createImagesFolder != FALSE) {
+    lapply(c(glue('Images ({verNum})')), function(x)
+      if (!dir.exists(x))
+        dir.create(
+          x,
+          showWarnings = TRUE,
+          recursive = FALSE,
+          mode = "0777"
+        ))
+    folderImages <<- file.path(glue('{getwd()}\\Images ({verNum})'))
+  }
 
-  if (createPulseFolder == TRUE) {
+  if (createPulseSubfolder != FALSE) {
     lapply(c(glue('Output ({verNum})\\Pulse')), function(x)
       if (!dir.exists(x))
         dir.create(
