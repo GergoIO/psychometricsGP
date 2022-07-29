40273b116007ea4c5fea3d0ec7cf3aa1f771eb77
Gergo <gergopinter@live.co.uk>
2022-07-29 11:10:29 +0100

    Reorder variable names

--- R/fnRptCaptionPlot.R
+++ R/fnRptCaptionPlot.R
@@ -1,8 +1,8 @@
 #' Add a new caption to accompany a plot and return the value of the total number of plots.
 #'
 #' @param report The variable name of the report
-#' @param caption The caption to add (string)
 #' @param plotCount The variable name in the document which stores the current number of plots already in the report (integer, usually named 'countPlt')
+#' @param caption The caption to add (string)
 #'
 #' @return The new value for the total number of plots is returned. The output of this function should be saved to match whatever that varialbe is stored as in the main script. In addition to returning this, the formatted caption is also added the variable saving the report
 #' @export
@@ -13,8 +13,9 @@
 
 fnRptCaptionPlot <-
   function(report = NULL,
-           caption = NULL,
-           plotCount = NULL) {
+           plotCount = NULL,
+           caption = NULL
+  ) {
     if (is.null(report) == TRUE |
         is.null(caption) == TRUE | is.null(plotCount) == TRUE)
     {
