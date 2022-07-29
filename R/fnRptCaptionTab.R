901893a0040b429ae6c0a87b52baf1340af99ff3
Gergo <gergopinter@live.co.uk>
2022-07-29 11:18:26 +0100

    adding new function to add a caption to tables

---
+++ R/fnRptCaptionTab.R
@@ -0,0 +1,31 @@
+#' Add a new caption to accompany a table and return the value of the total number of tables.
+#'
+#' @param report The variable name of the report
+#' @param tableCount The variable name in the document which stores the current number of tables already in the report (integer, usually named 'countTab')
+#' @param caption The caption to add (string)
+#'
+#' @return The new value for the total number of tables is returned. The output of this function should be saved to match whatever that varialbe is stored as in the main script. In addition to returning this, the formatted caption is also added the variable saving the report
+#' @export
+#'
+#' @examples countTab <- fnRptCaptionTab(rptVar = rpt$All, tableCount = countTab, caption = "This caption")
+
+################################################################################
+
+fnRptCaptionTab <-
+  function(report = NULL,
+           tableCount = NULL,
+           caption = NULL
+  ) {
+    if (is.null(report) == TRUE |
+        is.null(caption) == TRUE | is.null(tableCount) == TRUE)
+    {
+      stop("One of the required variables for this function has not been specified.")
+    } else{
+      report <-
+        body_add_par(report,
+                     glue('Figure {tableCount}: {caption}'),
+                     style = "caption")
+      tableCount <- tableCount + 1
+      return(tableCount)
+    }
+  } # END
