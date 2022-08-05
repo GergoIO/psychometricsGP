5dd36e8984da20e54608516095bbe4516c175de0
Gergo <gergopinter@live.co.uk>
2022-08-05 09:14:44 +0100

    new behaviour for adding a trailing line

--- R/fnRptAddPlot.R
+++ R/fnRptAddPlot.R
@@ -4,7 +4,7 @@
 #' @param plot The variable name of the plot
 #' @param plotCount The variable name in the document which stores the current number of plots already in the report (integer, usually named 'countPlt')
 #' @param caption The caption to add (string)
-#' @param addTrailingLine TRUE/FALSE
+#' @param stopTrailingLine TRUE/FALSE - decides whether to add an empty line below the plot (default is FALSE if undeclared)
 #'
 #' @return The plot is added with an accompanying formatted caption to the configured report. The variable storing the number of plots in the report is incremented by 1 and a single trailing line is added following the caption (if requested)
 #' @export
@@ -18,12 +18,10 @@ fnRptAddPlot <-
            plot = NULL,
            plotCount = NULL,
            caption = NULL,
-           addTrailingLine = NULL) {
+           stopTrailingLine = FALSE) {
     if (is.null(report) == TRUE | is.null(plot) == TRUE |
         is.null(caption) == TRUE |
-        is.null(plotCount) == TRUE |
-        is.null(addTrailingLine) == TRUE)
-    {
+        is.null(plotCount) == TRUE)    {
       stop("One of the required variables for this function has not been specified.")
     } else if (is.logical(report)) {
       stop("The variable 'addTrailingLine' must be set to either 'TRUE' or 'FALSE'.")
@@ -40,7 +38,9 @@ fnRptAddPlot <-
       # Ensure that the variable fed through as 'plotCount' is updated in main script
       # The below line already appears in 'fnRptCaptionPlot' - however it must be repeated in the top level function so that the 'plotCount' variable is updated in the main script too
       assign(deparse(substitute(plotCount)), plotCount + 1, envir = globalenv())
-      if (addTrailingLine == TRUE) {
+      if (stopTrailingLine == FALSE) {
+
+      } else if (stopTrailingLine %in% c(NULL, FALSE)) {
         fnRptAddParagraph(report)
       }
     }
