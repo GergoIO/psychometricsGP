dfc359215a9b139401df35c2b8f2a5a2c147810b
Gergo <gergopinter@live.co.uk>
2022-08-05 09:15:20 +0100

    new behaviour for adding a trailing line and forcing data to a flextable

--- R/fnRptAddTable.R
+++ R/fnRptAddTable.R
@@ -4,7 +4,8 @@
 #' @param table The variable name of the table
 #' @param tableCount The variable name in the document which stores the current number of tables already in the report (integer, usually named 'countTab')
 #' @param caption The caption to add (string)
-#' @param addTrailingLine TRUE/FALSE
+#' @param stopTrailingLine TRUE/FALSE - decides whether to add an empty line below the table (default is FALSE if undeclared)
+#' @param stopFlextableConversion TRUE/FALSE - decides whether to convert the input table to a flextable. If the table has already been converted to a flextable prior to its use in the function. Set stopFlextableCoversion to FALSE. Otherwise, if a dataframe is input to this function stopFlextableCoversion can be left undeclared (default is FALSE if undeclared)
 #'
 #' @return The table is added with an accompanying formatted caption to the configured report. The variable storing the number of tables in the report is incremented by 1 and a single trailing line is added following the caption (if requested)
 #' @export
@@ -18,11 +19,10 @@ fnRptAddTable <-
            table = NULL,
            tableCount = NULL,
            caption = NULL,
-           addTrailingLine = NULL) {
+           stopTrailingLine = FALSE) {
     if (is.null(report) == TRUE | is.null(table) == TRUE |
         is.null(caption) == TRUE |
-        is.null(tableCount) == TRUE |
-        is.null(addTrailingLine) == TRUE)
+        is.null(tableCount) == TRUE)
     {
       stop("One of the required variables for this function has not been specified.")
     } else if (is.logical(report)) {
@@ -36,11 +36,18 @@ fnRptAddTable <-
       # Ensure that the variable fed through as 'tableCount' is updated in main script
       # The below line already appears in 'fnRptCaptionTable' - however it must be repeated in the top level function so that the 'tableCount' variable is updated in the main script too
       assign(deparse(substitute(tableCount)), tableCount + 1, envir = globalenv())
+      if (stopFlextableConversion == TRUE) {
+        report <-
+          body_add_flextable(x = report,
+                             value = table)
+      } else if (stopFlextableConversion %in% c(NULL, FALSE)) {
+        report <-
+          body_add_flextable(x = report,
+                             value = qflextable(table))
+      }
+      if (stopTrailingLine == TRUE) {
 
-      report <-
-        body_add_flextable(x = report,
-                           value = qflextable(table))
-      if (addTrailingLine == TRUE) {
+      } else if (stopTrailingLine %in% c(NULL, FALSE)) {
         fnRptAddParagraph(report)
       }
     }
