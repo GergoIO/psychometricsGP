fdd41d14f9d63117325e00195838bfac6ec40571
Gergo <gergopinter@live.co.uk>
2022-08-24 14:11:29 +0100

    new fn to add a title

---
+++ R/fnRptSectionTitle.R
@@ -0,0 +1,19 @@
+#' Add a section title (level 2) to a report
+#'
+#' @param rptVar The variable name of the report
+#' @param sectionTitle This string will be the added section title
+#'
+#' @return Nothing is explicitly returned, rather the desired section title is added to the report (this is one level higher than fnRptSectionHeading)
+#' @export
+#'
+#' @examples fnRptSectionHeading(rptVar = rpt$All, headingTitle = "Grade Distributions")
+fnRptSectionHeading <- function(rptVar = NULL,
+                                sectionTitle = NULL) {
+  if (is.null(rptVar) == TRUE |
+      is.null(sectionTitle) == TRUE)
+  {
+    stop("One of the required variables for this function has not been specified.")
+  } else{
+    rptVar <- body_add_par(rptVar, sectionTitle, style = "heading 2")
+  }
+}
