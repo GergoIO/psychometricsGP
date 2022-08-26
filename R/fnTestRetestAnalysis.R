5add4e7b44f3db237fa1dc78a4b371688d948c06
Gergo <gergopinter@live.co.uk>
2022-08-26 14:03:03 +0100

    removal of matrix creation to separate fn (fnTestRetestMatrix)

--- R/fnTestRetestAnalysis.R
+++ R/fnTestRetestAnalysis.R
@@ -2,8 +2,8 @@
 #'
 #' @param stage A number or string (optional). If a stage is defined, the stage will be appended to the saved list items.  If a stage is not defined, no stage info will be appended.
 #' @param lstOfDetails A list - containing as least the following defined variables: assessment (the current assessment number - eg: PT36) and assessmentPrev (the previous assessment number - eg: PT35)
-#' @param dataAnalysis A list - the name of the variable storing all the test retest data analysis results. The output this function (fnTestRetestAnalysis) should be the same as the dataAnalysis variable. It is also defined as an input variable here so that prior data saved to this list is not overwritten. This list must be defined (even as an empty list eg: testRetest <- list()) in the main script prior to calling this function.
-#' @param dataRaw A dataframe - all the raw test retest data. When using this function for a specific stage, the input raw data must be for that stage only. No stage separation is performed as part of the function. The following columns must be present: "StudentID" and also the test scores and grades for the current assessement and the previous assessment in the form: "####_Score" and "####_Grade" (eg for analysis of PT36: "PT35_Score", "PT35_Grade", "PT36_Score", "PT36_Grade")
+#' @param dataAnalysis A list - the name of the variable storing all the test retest data analysis results. The output this function (fnTestRetestAnalysis) should be saved to the same dataAnalysis variable. It is also defined as an input variable here so that prior data saved to this list is not overwritten. This list must be defined (even as an empty list eg: testRetest <- list()) in the main script prior to calling this function.
+#' @param dataRaw A dataframe - all the raw test retest data. When using this function for a specific stage, the input raw data must be for that stage only. No stage separation is performed as part of the function. The following columns must be present: "StudentID" and also the test scores and grades for the current assessement and the previous assessment in the form: "####_Score" and (eg for analysis of PT36: "PT35_Score", PT36_Score")
 #'
 #' @return A list is returned. The list contains all the test retest analysis. Analysis is specific to whatever data is input. Variable names will contain the given stage if it is specified
 #' @export
@@ -31,9 +31,7 @@ fnTestRetestAnalysis <-
       assessmentPrev <- lstOfDetails$assessmentPrev
 
       testScore <- glue('{assessment}_Score')
-      testGrade <- glue('{assessment}_Grade')
       testScorePrev <- glue('{assessmentPrev}_Score')
-      testGradePrev <- glue('{assessmentPrev}_Grade')
 
       # Test Retest Cov
       # Save number of students considered
@@ -52,58 +50,6 @@ fnTestRetestAnalysis <-
       dataAnalysis[["corrHighCI"]] <-
         .looptestRetestCor$conf.int[2]
 
-      # Construct Test Retest Grade Matrix
-      dataAnalysis[["testGradePrev"]] <-
-        factor(
-          dataRaw[[testGradePrev]],
-          levels = c(
-            "Unsatisfactory",
-            "Borderline",
-            "Satisfactory",
-            "Excellent"
-          ),
-          ordered = TRUE
-        )
-      dataAnalysis[["testGrade"]] <-
-        factor(
-          dataRaw[[testGrade]],
-          levels = c(
-            "Unsatisfactory",
-            "Borderline",
-            "Satisfactory",
-            "Excellent"
-          ),
-          ordered = TRUE
-        )
-
-      testRetestMatrix <-
-        as.data.frame.matrix(table(dataAnalysis[["testGradePrev"]], dataAnalysis[["testGrade"]]))
-      rownames(testRetestMatrix) <- NULL
-
-      testRetestMatrix <-
-        cbind(
-          c(
-            "Unsatisfactory",
-            "Borderline",
-            "Satisfactory",
-            "Excellent"
-          ),
-          rep(assessmentPrev, 4),
-          testRetestMatrix
-        )
-      testRetestMatrix <-
-        rbind(c("Grade ↓",
-                "Assessment ↓→",
-                rep(assessment, 4)),
-              testRetestMatrix)
-      colnames(testRetestMatrix) <-
-        c(" ",
-          "Grade →",
-          "Unsatisfactory",
-          "Borderline",
-          "Satisfactory",
-          "Excellent")
-      dataAnalysis[["Matrix"]] <- testRetestMatrix
       return(dataAnalysis)
 
     } else {
@@ -134,58 +80,6 @@ fnTestRetestAnalysis <-
       dataAnalysis[[glue('corrHighCIStage{stage}')]] <-
         .looptestRetestCor$conf.int[2]
 
-      # Construct Test Retest Grade Matrix
-      dataAnalysis[[glue('{testGradePrev}Stage{stage}')]] <-
-        factor(
-          dataRaw[[testGradePrev]],
-          levels = c(
-            "Unsatisfactory",
-            "Borderline",
-            "Satisfactory",
-            "Excellent"
-          ),
-          ordered = TRUE
-        )
-      dataAnalysis[[glue('{testGrade}Stage{stage}')]] <-
-        factor(
-          dataRaw[[testGrade]],
-          levels = c(
-            "Unsatisfactory",
-            "Borderline",
-            "Satisfactory",
-            "Excellent"
-          ),
-          ordered = TRUE
-        )
-
-      testRetestMatrix <-
-        as.data.frame.matrix(table(dataAnalysis[[glue('{testGradePrev}Stage{stage}')]], dataAnalysis[[glue('{testGrade}Stage{stage}')]]))
-      rownames(testRetestMatrix) <- NULL
-
-      testRetestMatrix <-
-        cbind(
-          c(
-            "Unsatisfactory",
-            "Borderline",
-            "Satisfactory",
-            "Excellent"
-          ),
-          rep(assessmentPrev, 4),
-          testRetestMatrix
-        )
-      testRetestMatrix <-
-        rbind(c("Grade ↓",
-                "Assessment ↓→",
-                rep(assessment, 4)),
-              testRetestMatrix)
-      colnames(testRetestMatrix) <-
-        c(" ",
-          "Grade →",
-          "Unsatisfactory",
-          "Borderline",
-          "Satisfactory",
-          "Excellent")
-      dataAnalysis[[glue('MatrixStage{stage}')]] <- testRetestMatrix
       return(dataAnalysis)
     }
   }
