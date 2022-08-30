e1b0560b314c41c4eaa8e401d7519d1149301efa
Gergo <gergopinter@live.co.uk>
2022-08-30 13:32:03 +0100

    change rptVar to report

--- R/fnRptAddText.R
+++ R/fnRptAddText.R
@@ -1,21 +1,21 @@
 #' Add Text to a Report
 #'
-#' @param rptVar The variable name of the report
+#' @param report The variable name of the report
 #' @param text The text to add to the report
 #'
 #' @return Nothing is explicitly returned, rather the configured text is added to the configured report
 #' @export
 #'
-#' @examples fnRptAddText(rptVar = rpt$All, text = "Test text")
+#' @examples fnRptAddText(report = rpt$All, text = "Test text")
 
 ################################################################################
 
-fnRptAddText <- function(rptVar = NULL, text = NULL) {
-  if (is.null(rptVar) == TRUE | is.null(test) == TRUE)
+fnRptAddText <- function(report = NULL, text = NULL) {
+  if (is.null(report) == TRUE | is.null(text) == TRUE)
   {
     stop("One of the required variables for this function has not been specified.")
   } else{
-    rptVar <- body_add_par(rptVar,
+    report <- body_add_par(report,
                            text,
                            style = "Normal")
   }
