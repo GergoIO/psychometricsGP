65ccd395470968cdd8fc5a6c654b2bf1242c31ac
Gergo <gergopinter@live.co.uk>
2022-08-31 10:05:33 +0100

    fixed var "i" to "stage"

--- R/fnRptStageSpecificAnalysis.R
+++ R/fnRptStageSpecificAnalysis.R
@@ -18,6 +18,8 @@
 
 ################################################################################
 
+# Currently only testing on AMK
+
 fnRptStageSpecificAnalysis <- function(
   stage = NULL,
   report = NULL,
@@ -42,12 +44,12 @@ fnRptStageSpecificAnalysis <- function(
       is.null(countTab) == TRUE) {
     stop("fnRptStageSpecificAnalysis: One of the required variables for this function has not been specified.")
   } else{
-    fnRptSectionHeading(report, glue("Stage {i} Score Distribution"))
+    fnRptSectionHeading(report, glue("Stage {stage} Score Distribution"))
 
     fnRptAddText(
       report = report,
       text = glue(
-        "Scores for the {dim(dfResStagesOnlyScores[[glue('Stage {i}')]])[1]} Stage {i} students were {ifelse(tab[[glue('ShapiroStage{i}')]]$ShapiroP<0.05, 'not', '')} normally distributed (Shapiro-Wilk test, W={sprintf('%.3f', tab[[glue('ShapiroStage{i}')]]$ShapiroW)}, p={sprintf('%.3f', tab[[glue('ShapiroStage{i}')]]$ShapiroP)}."
+        "Scores for the {dim(dfResStagesOnlyScores[[glue('Stage {stage}')]])[1]} Stage {stage} students were {ifelse(tab[[glue('ShapiroStage{stage}')]]$ShapiroP<0.05, 'not', '')} normally distributed (Shapiro-Wilk test, W={sprintf('%.3f', tab[[glue('ShapiroStage{stage}')]]$ShapiroW)}, p={sprintf('%.3f', tab[[glue('ShapiroStage{stage}')]]$ShapiroP)}."
       )
     )
     fnRptAddParagraph(report)
@@ -55,17 +57,17 @@ fnRptStageSpecificAnalysis <- function(
     # PLT Score Distr. for STAGES B
     fnRptAddPlot(
       report = report,
-      plot = plt[[glue('HistogramScoresStage{i}')]],
+      plot = plt[[glue('HistogramScoresStage{stage}')]],
       plotCount = countPlt,
-      caption = glue('Distribution of Stage {i} test scores')
+      caption = glue('Distribution of Stage {stage} test scores')
     )
 
-    fnRptSectionHeading(report, glue("Stage {i} Test-Retest Statistics"))
+    fnRptSectionHeading(report, glue("Stage {stage} Test-Retest Statistics"))
 
     fnRptAddText(
       report = report,
       text = glue(
-        "There were {testRetest[[glue('nTestRetestStage{i}')]]} Stage {i} students for whom scores on both this test ({cnst$assessment}) and the previous test ({cnst$assessmentPrev}) were available. The Pearson correlation between the test scores was {sprintf('%.2f', testRetest[[glue('corrValStage{i}')]])} (95% confidence interval {sprintf('%.2f', testRetest[[glue('corrLowCIStage{i}')]])} to {sprintf('%.2f', testRetest[[glue('corrHighCIStage{i}')]])})."
+        "There were {testRetest[[glue('nTestRetestStage{stage}')]]} Stage {stage} students for whom scores on both this test ({cnst$assessment}) and the previous test ({cnst$assessmentPrev}) were available. The Pearson correlation between the test scores was {sprintf('%.2f', testRetest[[glue('corrValStage{stage}')]])} (95% confidence interval {sprintf('%.2f', testRetest[[glue('corrLowCIStage{stage}')]])} to {sprintf('%.2f', testRetest[[glue('corrHighCIStage{stage}')]])})."
       )
     )
     fnRptAddParagraph(report)
@@ -73,27 +75,27 @@ fnRptStageSpecificAnalysis <- function(
     # PLT Scatterplot of test score comparisons STAGES A
     fnRptAddPlot(
       report = report,
-      plot = plt[[glue('testRetestStage{i}')]],
+      plot = plt[[glue('testRetestStage{stage}')]],
       plotCount = countPlt,
-      caption = glue('Scatterplot of Stage {i} scores on current and previous test')
+      caption = glue('Scatterplot of Stage {stage} scores on current and previous test')
     )
 
     # TAB Test Retest Matrix STAGES A
     fnRptAddTable(
       report = report,
-      table = testRetest[[glue('MatrixStage{i}')]],
+      table = testRetest[[glue('MatrixStage{stage}')]],
       tableCount = countTab,
       caption = glue(
-        'Stage {i} students by grade awarded in {cnst$assessment} (row) and {cnst$assessmentPrev} (column)'
+        'Stage {stage} students by grade awarded in {cnst$assessment} (row) and {cnst$assessmentPrev} (column)'
       )
     )
 
-    fnRptSectionHeading(report, glue("Stage {i} Subgroup Analysis (ANOVA)"))
+    fnRptSectionHeading(report, glue("Stage {stage} Subgroup Analysis (ANOVA)"))
 
     fnRptAddText(
       report = report,
       text = glue(
-        "Analysis of variance of the scores by students gender, ethnicity and disability status showed{ifelse(any(tabDemog[[glue('AnovaStage{i}')]][['P-value']] < 0.05), ' ', ' no ')}statistically significant variation as shown in the table below."
+        "Analysis of variance of the scores by students gender, ethnicity and disability status showed{ifelse(any(tabDemog[[glue('AnovaStage{stage}')]][['P-value']] < 0.05), ' ', ' no ')}statistically significant variation as shown in the table below."
       )
     )
     fnRptAddParagraph(report)
@@ -101,7 +103,7 @@ fnRptStageSpecificAnalysis <- function(
     # TAB ANOVA STAGES B
     fnRptAddTable(
       report = report,
-      table = tabDemog[[glue('AnovaStage{i}')]],
+      table = tabDemog[[glue('AnovaStage{stage}')]],
       tableCount = countTab,
       caption = glue(
         "Analysis of variance table (Type III sums of squares, dependent variable: ({cnst$assessment} % Score))"
@@ -110,7 +112,7 @@ fnRptStageSpecificAnalysis <- function(
     # TAB Adj Means STAGES B
     fnRptAddTable(
       report = report,
-      table = tabDemog[[glue('MeansAdjStage{i}')]],
+      table = tabDemog[[glue('MeansAdjStage{stage}')]],
       tableCount = countTab,
       caption = glue(
         "Estimated marginal means (dependent variable: ({cnst$assessment} % Score))"
@@ -120,13 +122,13 @@ fnRptStageSpecificAnalysis <- function(
     fnRptAddText(
       report = report,
       text = glue(
-        "Score variation by entry pathway and origin cannot reliably be included in the above ANOVA due to low student numbers. The table below presents Stage {i} students' observed mean scores broken down by Entry Pathway and Origin."
+        "Score variation by entry pathway and origin cannot reliably be included in the above ANOVA due to low student numbers. The table below presents Stage {stage} students' observed mean scores broken down by Entry Pathway and Origin."
       )
     )
     # TAB Obs Means STAGES B
     fnRptAddTable(
       report = report,
-      table = tabDemog[[glue('MeansObsStage{i}')]],
+      table = tabDemog[[glue('MeansObsStage{stage}')]],
       tableCount = countTab,
       caption = glue("Observed mean percentage scores")
     )
