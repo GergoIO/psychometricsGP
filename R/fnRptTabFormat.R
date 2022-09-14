e840c8e0f29b70bbe0d0af0de3371edbdd0e3acc
Gergo <gergopinter@live.co.uk>
2022-09-14 15:29:52 +0100

    added option to not convert any rows or cols to integer

--- R/fnRptTabFormat.R
+++ R/fnRptTabFormat.R
@@ -4,8 +4,8 @@
 #'
 #' @param table (Dataframe) The input data, which will be formatted
 #' @param decimals (Integer, Numeric) The number of decimals to round values to
-#' @param editColumnsNotRows (Boolean) If TRUE, the specified COLUMN numbers will be converted back to integers. If FALSE, the specified ROW numbers will be converted back to integers.
-#' @param integerColumnsOrRows (Numeric, a list or single value eg: 1 or c(1,2,3)) The numbers of the COLUMNS or ROWS (depending on the editColumnsNotRows variable) which will be displayed as integers
+#' @param editColumnsNotRows (OPTIONAL, Boolean) If TRUE, the specified COLUMN numbers will be converted back to integers. If FALSE, the specified ROW numbers will be converted back to integers. If this variable is not defined, none of the coloumns of rows will be converted to integers
+#' @param integerColumnsOrRows (OPTIONAL, Numeric, a list or single value eg: 1 or c(1,2,3)) The numbers of the COLUMNS or ROWS (depending on the editColumnsNotRows variable) which will be displayed as integers. If this variable is not defined, none of the columns or rows will be converted to integers.
 #'
 #' @return The formatted table is returned
 #' @export
@@ -21,11 +21,14 @@ fnRptTabFormat <-
            editColumnsNotRows,
            integerColumnsOrRows) {
     if (is.null(table) == TRUE |
-        is.null(decimals) == TRUE |
-        is.null(editColumnsNotRows) == TRUE |
-        is.null(integerColumnsOrRows) == TRUE)
+        is.null(decimals) == TRUE)
     {
       stop("One of the required variables for this function has not been specified.")
+    } else if (any(is.null(editColumnsNotRows),
+                   is.null(integerColumnsOrRows)) == TRUE) {
+      stop(
+        'Only one of the variables ("editColumnsNotRows" or "integerColumnsOrRows") has not been defined. To convert some rounded columns or rows back to integer, both variables must be defined. Alternatively, if neither variable is defined, then no columns or rows are converted back to integer following rounding.'
+      )
     } else{
       # Convert table to numeric
       table <- mutate_all(table, function(x)
@@ -35,14 +38,19 @@ fnRptTabFormat <-
       table <-
         table %>% format(round(decimals), signif(decimals), nsmall = decimals)
 
-      # Convert required cols/rows to integer
-      if (editColumnsNotRows == TRUE) {
-        table[, integerColumnsOrRows] <-
-          sapply(table[, integerColumnsOrRows], as.integer)
-      } else{
-        table[integerColumnsOrRows, ] <-
-          sapply(table[integerColumnsOrRows, ], as.integer)
+      # Check if columns or rows should be converted to integer
+      if (is.null(integerColumnsOrRows) == FALSE) {
+        # Convert required cols/rows to integer
+        if (editColumnsNotRows == TRUE) {
+          # Convert specified COLUMNS to integer
+          table[, integerColumnsOrRows] <-
+            sapply(table[, integerColumnsOrRows], as.integer)
+        } else{
+          # Convert specified ROWS to integer
+          table[integerColumnsOrRows,] <-
+            sapply(table[integerColumnsOrRows,], as.integer)
+        }
+        return(table)
       }
-      return(table)
     }
   }
