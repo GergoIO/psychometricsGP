bfab25df8521f3f6af43f731cb57cbf538b4376b
Gergo <gergopinter@live.co.uk>
2022-09-27 14:04:02 +0100

    Changes to the way saving plots is configured, this can now be explicitly defined using just one var - savePlot
    no need for controls var anymore
    
    dimensions is now optional with the 15x15cm as the default now

--- R/fnPltSave.R
+++ R/fnPltSave.R
@@ -1,45 +1,30 @@
 #' Save a plot
 #'
-#' @param controls A list containing at least the defined (TRUE/FALSE) variables 'stopAllOutputs' and 'stopImageSaving'. If either of the variables are TRUE, this function will not save an image. Otherwise, if both variables are FALSE, an image will be saved. If 'controls' is not defined, the default behaviour is to proceed and save an image if possible. The user will be notified in this instance.
-#' @param plot The variable name of the plot
-#' @param dimensions A vector containing the width, height and units of the dimensions of the saved image
+#' @param savePlot A boolean (TRUE/FALSE) determines whether the plot will be saved. Useful for controlling overall save behaviour.
+#' @param plot The variable name of the plot - must exist in your environment
+#' @param dimensions A vector containing the width, height and units of the dimensions of the saved image (in that order). If not set, defaults to 15cm x 15cm
 #' @param filePath The file path where the plot is saved
 #'
 #' @return Nothing is explicitly returned, a plot is saved
 #' @export
 #'
-#' @examples fnPltSave(controls = list(stopAllOutputs = TRUE, stopImageSaving = FALSE), plot = plt1, filePath = glue('{some path}\\filename.jpg'))
+#' @examples fnPltSave(savePlot = TRUE, plot = plt1, dimensions = c(15, 15, "cm"), filePath = glue('{some path}\\filename.jpg'))
 
 ################################################################################
 
 fnPltSave <-
-  function(controls = NULL,
+  function(savePlot = NULL,
            plot = NULL,
-           dimensions = NULL,
+           dimensions = c(15, 15, "cm"),
            filePath = NULL) {
     if (is.null(plot) == TRUE |
-        is.null(filePath) == TRUE | is.null(dimensions) == TRUE)    {
+        is.null(filePath) == TRUE) {
       stop("One of the required variables for this function has not been specified.")
-    } else if (is.null(controls) == TRUE) {
-      message("fnPltSave: The controls variable has not been set. Proceeding with default values")
-      controls = list()
-      controls$stopAllOutputs <- FALSE
-      controls$stopImageSaving <- FALSE
+    } else if (is.null(savePlot) == TRUE) {
+      message("fnPltSave: The 'savePlot' variable has not been set. Defaulting to saving the plot.")
+      savePlot <- TRUE
     }
-    else if (is.null(controls$stopAllOutputs) == TRUE) {
-      message(
-        "fnPltSave: The control list does not contain a 'stopAllOutputs' variable. Proceeding with the default of false"
-      )
-      conrols$stopAllOutputs <- FALSE
-    }
-    else if (is.null(controls$stopImageSaving) == TRUE) {
-      message(
-        "fnPltSave: The control list does not contain a 'stopAllOutputs' variable. Proceeding with the default of false"
-      )
-      conrols$stopImageSaving <- FALSE
-    }
-    else if (controls$stopAllOutputs == FALSE &&
-             controls$stopImageSaving == FALSE) {
+    else if (savePlot == TRUE) {
       ggsave(
         plot,
         file = filePath,
@@ -49,3 +34,42 @@ fnPltSave <-
       )
     }
   }
+
+# OLD version
+# fnPltSave <-
+#   function(controls = NULL,
+#            plot = NULL,
+#            dimensions = NULL,
+#            filePath = NULL) {
+#     if (is.null(plot) == TRUE |
+#         is.null(filePath) == TRUE | is.null(dimensions) == TRUE)    {
+#       stop("One of the required variables for this function has not been specified.")
+#     } else if (is.null(controls) == TRUE) {
+#       message("fnPltSave: The controls variable has not been set. Proceeding with default values")
+#       controls = list()
+#       controls$stopAllOutputs <- FALSE
+#       controls$stopImageSaving <- FALSE
+#     }
+#     else if (is.null(controls$stopAllOutputs) == TRUE) {
+#       message(
+#         "fnPltSave: The control list does not contain a 'stopAllOutputs' variable. Proceeding with the default of FALSE"
+#       )
+#       conrols$stopAllOutputs <- FALSE
+#     }
+#     else if (is.null(controls$stopImageSaving) == TRUE) {
+#       message(
+#         "fnPltSave: The control list does not contain a 'stopAllOutputs' variable. Proceeding with the default of FALSE"
+#       )
+#       conrols$stopImageSaving <- FALSE
+#     }
+#     else if (controls$stopAllOutputs == FALSE &&
+#              controls$stopImageSaving == FALSE) {
+#       ggsave(
+#         plot,
+#         file = filePath,
+#         width = as.numeric(dimensions[1]),
+#         height = as.numeric(dimensions[2]),
+#         units = c(dimensions[3])
+#       )
+#     }
+#   }
