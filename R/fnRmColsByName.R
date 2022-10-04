b720be1e99c2e8d0a81f2527a1cd16c7f5c7b431
Gergo <gergopinter@live.co.uk>
2022-10-04 10:07:53 +0100

    Updating wording and removing defunct 'continue' variable

--- R/fnRmColsByName.R
+++ R/fnRmColsByName.R
@@ -1,23 +1,23 @@
-#' Removes specific columns by name from a dataframe. Additional functionality to handle scenarios where no columns need to be removed.
+#' Remove Rows By Name
+#'
+#' @description Removes specific columns by name from a dataframe. Additional functionality to handle scenarios where no columns need to be removed.
 #'
 #' @param df The name of the data frame.
-#' @param itemsToRm The items to remove. Can be an empty vector
+#' @param itemsToRm (Vector of numerics) The items to remove. Can be an empty vector, c()
 #'
 #' @return This function will return a dataframe with the selected columns removed
 #' @export
 #'
-#' @examples fnRmColsByName(dfName, c(15,29,43))
+#' @examples fnRmColsByName(dfName, c(15,29,43)), fnRmColsByName(dfName, c())
 
 ################################################################################
 
 fnRmColsByName <- function(df, itemsToRm) {
-  continue <- TRUE
   if (is.null(df) == TRUE) {
-    continue <- FALSE
     stop("Please specify the dataframe")
-  } else if (is.null(itemsToRm) == TRUE) {
-    continue <- FALSE
-    warning("There are no items to remove, no columns will be deleted")
+  }
+  if (is.null(itemsToRm) == TRUE) {
+    message("There are no items to remove, no columns will be deleted")
     return(df)
   } else {
     return(df[, -itemsToRm])
--- R/fnRmRowsByName.R
+++ R/fnRmRowsByName.R
@@ -1,23 +1,24 @@
-#' Removes specific columns by name from a dataframe. Additional functionality to handle scenarios where no columns need to be removed. 1D lists and 2D data frames are handled
+#' Remove Rows By Name
+#'
+#' @description Removes specific rows by name from a dataframe or list. Additional functionality to handle scenarios where no columns need to be removed. Works for 1D lists and 2D data frames.
 #'
 #' @param input The name of the data frame or list.
-#' @param itemsToRm The items to remove. Can be an empty vector c()
+#' @param itemsToRm (A vector of numerics) The items to remove. Can be an empty vector, c()
 #'
 #' @return This function will return a dataframe with the selected rows removed
 #' @export
 #'
-#' @examples fnRmRowsByName(df, c(2)), fnRmRowsByName(df, c()), fnRmRowsByName(list, c(1,2,3))
+#' @examples fnRmRowsByName(dfName, c(2)), fnRmRowsByName(dfName, c()), fnRmRowsByName(list, c(1,2,3))
 
 ################################################################################
 
 fnRmRowsByName <- function(input, itemsToRm) {
   continue <- TRUE
   if (is.null(input) == TRUE) {
-    continue <- FALSE
-    stop("Please specify the data")
-  } else if (is.null(itemsToRm) == TRUE) {
-    continue <- FALSE
-    warning("There are no items to remove, no rows will be deleted")
+    stop("fnRmRowsByName: No data is specified. Please specify the data")
+  }
+  if (is.null(itemsToRm) == TRUE) {
+    message("There are no items to remove, no rows will be deleted")
     return(input)
   } else if (is.null(dim(input)) == TRUE) {
     return(input[-itemsToRm]) # for 1D lists etc
