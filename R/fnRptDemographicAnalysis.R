0dfdc5ea46fd8c4cbc7cd3c5bbd97ea477dfc179
Gergo <gergopinter@live.co.uk>
2022-10-05 15:43:21 +0100

    new fn for adding demog anova info to reports

---
+++ R/fnRptDemographicAnalysis.R
@@ -0,0 +1,73 @@
+#' ANOVA for Report
+#'
+#' @param report ***
+#' @param cnst ***
+#' @param plt ***
+#' @param tab ***
+#' @param tabDemog ***
+#' @param tableCount ***
+#'
+#' @return ***
+#' @export
+#'
+#' @examples ***
+fnRptDemographicAnalysis <- function(report = NULL,
+                                       cnst = NULL,
+                                       plt = NULL,
+                                       tab = NULL,
+                                       tabDemog = NULL,
+                                       tableCount = NULL) {
+  if (is.null(report) == TRUE |
+      is.null(cnst) == TRUE |
+      is.null(plt) == TRUE |
+      is.null(tab) == TRUE |
+      is.null(tabDemog) == TRUE |
+      is.null(tableCount) == TRUE) {
+    stop(
+      "fnRptDemographicAnalysis: One of the required variables for this function has not been specified."
+    )
+  } else{
+    fnRptSectionHeading(report, glue("Demographic Analysis"))
+
+    fnRptAddText(
+      report = report,
+      text = glue(
+        "Analysis of variance of the scores by students gender, ethnicity and disability status showed{ifelse(any(tabDemog$Anova[['P-value']] < 0.05), ' ', ' no ')}statistically significant variation as shown in the table below."
+      )
+    )
+    fnRptAddParagraph(report)
+
+    # TAB ANOVA
+    fnRptAddTable(
+      report = report,
+      table = tabDemog$AnovaStage,
+      tableCount = tableCount,
+      caption = glue(
+        "Analysis of variance table (Type III sums of squares, dependent variable: ({cnst$assessment} % Score))"
+      )
+    )
+    # TAB Adj Means
+    fnRptAddTable(
+      report = report,
+      table = tabDemog$MeansAdjStage,
+      tableCount = tableCount,
+      caption = glue(
+        "Estimated marginal means (dependent variable: ({cnst$assessment} % Score))"
+      )
+    )
+
+    fnRptAddText(
+      report = report,
+      text = glue(
+        "Score variation by entry pathway and origin cannot reliably be included in the above ANOVA due to low student numbers. The table below presents students' observed mean scores broken down by Entry Pathway and Origin."
+      )
+    )
+    # TAB Obs Means
+    fnRptAddTable(
+      report = report,
+      table = tabDemog$MeansObs,
+      tableCount = tableCount,
+      caption = glue("Observed mean percentage scores")
+    )
+  }
+}
