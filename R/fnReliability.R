2f5f9acbe4746ce9abe9dc3b2fa06e9608531aa4
Gergo <gergopinter@live.co.uk>
2022-10-06 09:23:08 +0100

    changes to var names for single stage reliability calcs

--- R/fnReliability.R
+++ R/fnReliability.R
@@ -2,7 +2,7 @@
 #'
 #' @param stages (Optional) Vector containing all the stages being considered. If undefined, only a single dataset (scoresData) is expected/considered.
 #' @param scoresData If stage specific: A list of dataframes with students scores for each stage. Each data frame in the list should be named "Stage #" where # is the relevant stage.
-#'    If non stage specific: A single dataframe contaning what would otherwise be contained in one of the dataframes in the list of stage specific dataframes. In any dataframe the columns should be the items and the rows should be the index of each student (actual value unimportant). Each data point should represent the score for a particular student on a particular item. All students with NA scores and excluded items should be removed prior to use in this function
+#'    If non stage specific: A single dataframe containing what would otherwise be contained in one of the dataframes in the list of stage specific dataframes. In any dataframe the columns should be the items and the rows should be the index of each student (actual value unimportant). Each data point should represent the score for a particular student on a particular item. All students with NA scores and excluded items should be removed prior to use in this function
 #' @param resultsData A dataframe containing student results. This variable must contain a "Stage" column in which the stage of each student is entered in a new row. This is used to add the number of students assessed in each Stage to the resultant export of reliability data.
 #'
 #' @return A dataframe containing the reliability data for each considered stage is returned
@@ -23,20 +23,18 @@ fnReliability <-  function(stages = NULL,
   }
   if (is.null(stages) == TRUE) {
     message("fnReliability: Analysing reliability across a single dataset (single stage)")
-    loopDf <- scoresData
-
-    loopScoresLong <-
-      transform(loopDf, Candidate = c(1:nrow(loopDf)))
-    loopScoresLong <-
-      melt(loopScoresLong, id = "Candidate")
-    colnames(loopScoresLong) <- c("Candidate", "Item", "Score")
-    loopScoresLong$Item <-
-      gsub("X", "", loopScoresLong$Item, fixed = TRUE)
+    scoresLong <-
+      transform(scoresData, Candidate = c(1:nrow(scoresData)))
+    scoresLong <-
+      melt(ScoresLong, id = "Candidate")
+    colnames(ScoresLong) <- c("Candidate", "Item", "Score")
+    scoresLong$Item <-
+      gsub("X", "", scoresLong$Item, fixed = TRUE)
 
     fitLinearMixEffectModel <-
       lmer(Score ~ (1 |
                       Candidate) + (1 |
-                                      Item), data = loopScoresLong)
+                                      Item), data = scoresLong)
 
     relCalcs$varCorr <-
       as.data.frame(VarCorr(fitLinearMixEffectModel))
@@ -74,7 +72,7 @@ fnReliability <-  function(stages = NULL,
         "Phi Coefficient",
         "Absolute SEM"
       )
-    reliability[,1] <- c(
+    reliability[, 1] <- c(
       dim(resultsData)[1],
       cronbach.alpha(scoresData)[[1]],
       relCalcs$varianceCandidate,
@@ -142,7 +140,7 @@ fnReliability <-  function(stages = NULL,
           "Absolute SEM"
         )
       reliability[, stage] <- c(
-        dim(resultsData[resultsData$Stage == i,])[1],
+        dim(resultsData[resultsData$Stage == i, ])[1],
         cronbach.alpha(scoresData[[stage]])[[1]],
         relCalcs$varianceCandidate,
         relCalcs$varianceItem,
