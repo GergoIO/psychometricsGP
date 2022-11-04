863a20b1c7243da9f39f812d0efa28c9503a337b
Gergo <gergopinter@live.co.uk>
2022-11-04 09:56:40 +0000

    new fn to reload this package

---
+++ R/fnReload.R
@@ -0,0 +1,22 @@
+#' Reload the PsychometricGP package
+#'
+#' @description Use this function to reload the current package (psychometricsGP). This function is useful when changes have been made and uploaded to the package repository. This function takes no arguments.
+#'
+#' @return Nothing is returned
+#' @export
+#'
+#' @examples fnReload()
+#'
+################################################################################
+#'
+fnReload <- function() {
+  # Detach the package first
+  detach("package:psychometricsGP", unload = TRUE)
+  # Reload the package from the online repo
+  devtools::install_github("GergoIO/psychometricsGP",
+                           # Hidden variable from the secrets file
+                           auth_token = .githubToken,
+                           quiet = TRUE)
+  # Start using the custom package
+  library(psychometricsGP)
+}
