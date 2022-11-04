db863041fba081b3bb157a0efd02e441cbccdd6d
Gergo <gergopinter@live.co.uk>
2022-11-04 09:37:45 +0000

    added new default dimensions to image but allows it to be overriden easily

--- R/fnRptAddPlot.R
+++ R/fnRptAddPlot.R
@@ -5,6 +5,7 @@
 #' @param plotCount The variable name in the document which stores the current number of plots already in the report (integer, usually named 'countPlt')
 #' @param caption The caption to add (string)
 #' @param stopTrailingLine TRUE/FALSE - decides whether to add an empty line below the plot (default is FALSE if undeclared)
+#' @param dimensions (OPTIONAL) A vector defining the width and height (in cm) for the image. Defaults to 18cm wide and 12cm high
 #'
 #' @return The plot is added with an accompanying formatted caption to the configured report. The variable storing the number of plots in the report is incremented by 1 and a single trailing line is added following the caption (if requested)
 #' @export
@@ -18,16 +19,20 @@ fnRptAddPlot <-
            plot = NULL,
            plotCount = NULL,
            caption = NULL,
-           stopTrailingLine = FALSE) {
+           stopTrailingLine = FALSE,
+           dimensions = c(18, 12)) {
     if (is.null(report) == TRUE | is.null(plot) == TRUE |
         is.null(caption) == TRUE |
         is.null(plotCount) == TRUE)    {
       stop("One of the required variables for this function has not been specified.")
     } else{
-      report <- body_add_gg(report,
-                            value = plot,
-                            height = 4,
-                            width = 6)
+      # The /2.54 is to convert from the defined cm to inches which the body_add_gg requires
+      report <- body_add_gg(
+        report,
+        value = plot,
+        width = as.numeric(dimensions[1] / 2.54),
+        height = as.numeric(dimensions[2] / 2.54)
+      )
       # Caption goes below the plot
       fnRptCaptionPlot(report = report,
                        plotCount = plotCount,
