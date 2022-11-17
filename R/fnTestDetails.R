09e8fc0fa7007451864db8e3be1efcbdb6234f3d
Gergo <gergopinter@live.co.uk>
2022-11-17 08:36:15 +0000

    removed use of cnst for programme and introduce standalone variable

--- R/fnTestDetails.R
+++ R/fnTestDetails.R
@@ -1,6 +1,7 @@
 #' Test Details Parameters (Stage Specific)
 #'
 #' @param stages A vector - denoting the stages to be considered. (stages = 2 or stages = c(2,3,4,5) etc.)
+#' @param programme A string - the programme of the students (Must match what is in the "Programme" col in the demogData dataframe (see below) eg "BMBS" etc.)
 #' @param demogData A dataframe - containing demographics data columns. Columns titled "Programme", "Stage" and "Year_Status" must be included. Make sure to use a df containing demographics data for all students and not one which has been filtered to include present students only
 #' @param results A dataframe - containing results for students. A column titled "Stage" must be included. Each student should have one entry (row) in this dataframe. This will be used to determine the number of students assessed in each stage
 #' @param resultsAbsent A dataframe - containing results for absent students. A column titled "Stage" must be included. Each absent student should have one entry (row) in this dataframe. This will be used to determine the number of absent students who were not assessed in each stage
@@ -8,16 +9,18 @@
 #' @return A dataframe is returned (testDetails) containing the calculated test details. A new column is generated for each stage considered
 #' @export
 #'
-#' @examples fnTestDetails(stages = cnst$stages, demog = dfDemog, results = dfRes, resultsAbsent = dfResAbsent)
+#' @examples fnTestDetails(stages = cnst$stages, programme = cnst$programme, demog = dfDemog, results = dfRes, resultsAbsent = dfResAbsent)
 
 ################################################################################
 
 fnTestDetails <-
   function(stages = NULL,
+           programme = NULL,
            demogData = NULL,
            results = NULL,
            resultsAbsent = NULL) {
     if (is.null(stages) == TRUE |
+        is.null(programme) == TRUE |
         is.null(demogData) == TRUE |
         is.null(results) == TRUE | is.null(resultsAbsent) == TRUE) {
       stop("One of the required variables for this function has not been specified.")
@@ -28,7 +31,7 @@ fnTestDetails <-
         testDetails["Students in Stage", stage] <-
           length(
             which(
-              demogData$Programme == cnst$programme &
+              demogData$Programme == programme &
                 demogData$Stage == i &
                 demogData$Year_Status %!in% c("Interrupted", "Withdrawn")
             )
