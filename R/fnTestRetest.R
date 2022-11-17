9ffea3936089564ad7d50eceeaf68b33e256db43
Gergo <gergopinter@live.co.uk>
2022-11-17 11:13:02 +0000

    removed use of list of details and replaced with explicitly defined current and previous assessment names

--- R/fnTestRetest.R
+++ R/fnTestRetest.R
@@ -1,24 +1,27 @@
 #' Test Retest Dataframe Creation
 #'
-#' @param listOfDetails This variable should be a list containing as least the following defined variables: assessment (the current assessment number - eg: PT36) and assessmentPrev (the previous assessment number - eg: PT35)
-#' @param results A dataframe - containing results data for the current assessment with at least the following columns: "StudentID", "Stage", assessement score (of the form "assessement_Score" where assessment matches the variable defined in listOfDetails) and assessment grade (of the form "assessment_Grade" where assessment grade matched the variable defined in listOfDetails)
+#' @param assessment A string - the current assessment being analysed (eg ADK38)
+#' @param assessmentPrev A string - the previous assessment (prior to the one being analysed - eg ADK37)
+#' @param results A dataframe - containing results data for the current assessment with at least the following columns: "StudentID", "Stage", assessement score (of the form "assessement_Score" where assessment matches the variable defined in the "assessment" parameter) and assessment grade (of the form "assessment_Grade" where assessment matched the variable defined in the "assessment" parameter)
 #' @param resultsPrevious A dataframe - containing results data for the previous assessment, in a similar fashion to the "results" dataframe
 #'
 #' @return A dataframe is returned. It contains the student ID, stage and current and previous assessment grades and scores.
 #' @export
 #'
-#' @examples dfTestRetest <- fnTestRetest(listOfDetails = cnst, results = dfRes, resultsPrevious = dfPrevTestData)
+#' @examples dfTestRetest <- fnTestRetest(assessment = cnst$assessment, assessmentPrev = cnst$assessmentPrev, results = dfRes, resultsPrevious = dfPrevTestData)
 
 ################################################################################
 # Used in:
-# AMK
+# AMK, ADK
 
 fnTestRetest <-
-  function(listOfDetails = NULL,
+  function(assessment = NULL,
+           assessmentPrev = NULL,
            results = NULL,
            resultsPrevious = NULL)
   {
-    if (is.null(listOfDetails) == TRUE |
+    if (is.null(assessment) == TRUE |
+        is.null(assessmentPrev) == TRUE |
         is.null(results) == TRUE |
         is.null(resultsPrevious) == TRUE) {
       stop("One of the required variables for this function has not been specified.")
@@ -40,19 +43,19 @@ fnTestRetest <-
 
       #Remove any rows with NA for present or previous test score
       testRetest <-
-        testRetest[!is.na(testRetest[[glue('{listOfDetails$assessment}_Score')]]),]
+        testRetest[!is.na(testRetest[[glue('{assessment}_Score')]]),]
       testRetest <-
-        testRetest[!is.na(testRetest[[glue('{listOfDetails$assessmentPrev}_Score')]]),]
+        testRetest[!is.na(testRetest[[glue('{assessmentPrev}_Score')]]),]
 
       # Only keep the required columns
       testRetest <-
         testRetest[, c(
           "StudentID",
           "Stage",
-          glue('{listOfDetails$assessment}_Score'),
-          glue('{listOfDetails$assessment}_Grade'),
-          glue('{listOfDetails$assessmentPrev}_Score'),
-          glue('{listOfDetails$assessmentPrev}_Grade')
+          glue('{assessment}_Score'),
+          glue('{assessment}_Grade'),
+          glue('{assessmentPrev}_Score'),
+          glue('{assessmentPrev}_Grade')
         )]
       return(testRetest)
     }
