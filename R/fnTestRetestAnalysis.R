a84ded317e6411a8ad06b02d1db351f300d84acc
Gergo <gergopinter@live.co.uk>
2022-11-18 13:43:32 +0000

    removed lstOfDetails replaced with explicity assessment and assessmentPrev vars. small ammends to code to remove bits no longer used

--- R/fnTestRetestAnalysis.R
+++ R/fnTestRetestAnalysis.R
@@ -1,23 +1,26 @@
 #' Test Retest Data Analysis
 #'
-#' @param stage A number or string (optional). If a stage is defined, the stage will be appended to the saved list items.  If a stage is not defined, no stage info will be appended.
-#' @param lstOfDetails A list - containing as least the following defined variables: assessment (the current assessment number - eg: PT36) and assessmentPrev (the previous assessment number - eg: PT35)
+#' @param stage A number or string (OPTIONAL). If a stage is defined, the stage will be appended to the saved list items.  If a stage is not defined, no stage info will be appended.
+#' @param assessment A string - the current assessment being analysed (eg ADK38)
+#' @param assessmentPrev A string - the previous assessment (prior to the one being analysed - eg ADK37)
 #' @param dataAnalysis A list - the name of the variable storing all the test retest data analysis results. The output this function (fnTestRetestAnalysis) should be saved to the same dataAnalysis variable. It is also defined as an input variable here so that prior data saved to this list is not overwritten. This list must be defined (even as an empty list eg: testRetest <- list()) in the main script prior to calling this function.
 #' @param dataRaw A dataframe - all the raw test retest data. When using this function for a specific stage, the input raw data must be for that stage only. No stage separation is performed as part of the function. The following columns must be present: "StudentID" and also the test scores and grades for the current assessement and the previous assessment in the form: "####_Score" and (eg for analysis of PT36: "PT35_Score", PT36_Score")
 #'
 #' @return A list is returned. The list contains all the test retest analysis. Analysis is specific to whatever data is input. Variable names will contain the given stage if it is specified
 #' @export
 #'
-#' @examples testRetest <- fnTestRetestAnalysis(stage = 1, lstOfDetails = cnst, dataAnalysis = testRetest, dataRaw = dfResTestRetestStages[["Stage 1"]])
+#' @examples testRetest <- fnTestRetestAnalysis(stage = 1, assessment = cnst$assessment, assessmentPrev = cnst$assessmentPrev, dataAnalysis = testRetest, dataRaw = dfResTestRetestStages[["Stage 1"]])
 
 ################################################################################
 
 fnTestRetestAnalysis <-
   function(stage = NULL,
-           lstOfDetails = NULL,
+           assessment = NULL,
+           assessmentPrev = NULL,
            dataAnalysis = NULL,
            dataRaw = NULL) {
-    if (is.null(lstOfDetails) == TRUE |
+    if (is.null(assessment) == TRUE |
+        is.null(assessmentPrev) == TRUE |
         is.null(dataAnalysis) == TRUE |
         is.null(dataRaw) == TRUE) {
       stop("One of the required variables for this function has not been specified.")
@@ -27,11 +30,8 @@ fnTestRetestAnalysis <-
         "fnTestRetestAnalysis: No stage is specified. Continuing without stage specificity"
       )
 
-      assessment <- lstOfDetails$assessment
-      assessmentPrev <- lstOfDetails$assessmentPrev
-
-      testScore <- glue('{assessment}_Score')
-      testScorePrev <- glue('{assessmentPrev}_Score')
+      .testScore <- glue('{assessment}_Score')
+      .testScorePrev <- glue('{assessmentPrev}_Score')
 
       # Test Retest Cov
       # Save number of students considered
@@ -40,7 +40,7 @@ fnTestRetestAnalysis <-
       # Perform correlation test
       .looptestRetestCor <-
         dataAnalysis[["corrTest"]] <-
-        cor.test(dataRaw[[testScorePrev]], dataRaw[[testScore]])
+        cor.test(dataRaw[[.testScorePrev]], dataRaw[[.testScore]])
 
       # Save correlation test results
       dataAnalysis[["corrVal"]] <-
@@ -55,13 +55,8 @@ fnTestRetestAnalysis <-
     } else {
       message(glue("fnTestRetestAnalysis: A Stage ({stage}) is specified"))
 
-      assessment <- lstOfDetails$assessment
-      assessmentPrev <- lstOfDetails$assessmentPrev
-
-      testScore <- glue('{assessment}_Score')
-      testGrade <- glue('{assessment}_Grade')
-      testScorePrev <- glue('{assessmentPrev}_Score')
-      testGradePrev <- glue('{assessmentPrev}_Grade')
+      .testScore <- glue('{assessment}_Score')
+      .testScorePrev <- glue('{assessmentPrev}_Score')
 
       # Test Retest Cov
       # Save number of students considered
@@ -70,7 +65,7 @@ fnTestRetestAnalysis <-
       # Perform correlation test
       .looptestRetestCor <-
         dataAnalysis[[glue('corrTestStage{stage}')]] <-
-        cor.test(dataRaw[[testScorePrev]], dataRaw[[testScore]])
+        cor.test(dataRaw[[.testScorePrev]], dataRaw[[.testScore]])
 
       # Save correlation test results
       dataAnalysis[[glue('corrValStage{stage}')]] <-
