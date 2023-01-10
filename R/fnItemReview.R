81a4bb7f1fe3d0973253707c5b2718c2f20ea3df
Gergo <gergopinter@live.co.uk>
2023-01-10 12:17:20 +0000

    added AMK process and optional var - testInYear

--- R/fnItemReview.R
+++ R/fnItemReview.R
@@ -2,6 +2,7 @@
 #'
 #' @param assessmentType A string. The type of assessment being analysed. This will determine the selection process for finding and returning items to review
 #' @param itemAnalysisData A dataframe. This dataframe must contain at least the following columns: "Item" (the items number), "Stage 5 Facility" (for ADK), "Stage 5 vs 2 Growth" (for ADK), "Stage 5 PtBis" (for ADK) or "Stage 3 Facility" (for ADTK), "Stage 3 vs 2 Growth" (for ADTK), "Stage 3 PtBis" (for ADTK)
+#' @param testInYear (OPTIONAL) - Required for AMK assessments so the stage to use for item review can be determined
 #'
 #' @return A list of the detected items to review is returned.
 #' @export
@@ -12,18 +13,25 @@
 #'   fnItemReview(assessmentType = cnst$assessmentType, itemAnalysisData = tab$itemAnalysis)
 #'   )
 #'
+#' # For AMK:
+#'  cnst <- append(
+#'  cnst,
+#'   fnItemReview(assessmentType = cnst$assessmentType, itemAnalysisData = tab$itemAnalysis, testInYear = cnst$testInYear)
+#'   )
+#'
 ################################################################################
 #'
 fnItemReview <- function(assessmentType = NULL,
-                         itemAnalysisData = NULL) {
+                         itemAnalysisData = NULL,
+                         testInYear = NULL) {
   if (is.null(assessmentType) |
       is.null(itemAnalysisData)) {
     stop("One of the required variables for this function has not been specified.")
   } else{
     listOfItemReview <- list()
-
     #   ________________________________________________________________________
     #   ADK                                                                 ####
+
     if (assessmentType %in% c("ADK")) {
       # Low (<20%) Facility
       listOfItemReview$itemReviewLowFac <-
@@ -51,6 +59,7 @@ fnItemReview <- function(assessmentType = NULL,
     } else if (assessmentType %in% c("ADTK")) {
       #   ______________________________________________________________________
       #   ADTK                                                              ####
+
       # Low (<20%) Facility
       listOfItemReview$itemReviewLowFac <-
         unique(sort(as.numeric(c(
@@ -74,6 +83,43 @@ fnItemReview <- function(assessmentType = NULL,
         ))))
       listOfItemReview$itemReviewNegPtBis <-
         toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+    } else if (assessmentType %in% c("AMK")) {
+      #   ______________________________________________________________________
+      #   AMK                                                               ####
+
+      if (is.null(testInYear)) {
+        stop("For AMK assessments, the test in year (testInYear) variable must be defined.")
+      } else {
+        if (testInYear %in% c(1, 2)) {
+          # Low (<20%) Facility - Stage 5
+          listOfItemReview$itemReviewLowFac <-
+            unique(sort(as.numeric(c(
+              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Facility"]] < 0.2])
+            ))))
+          # Negative PtBis Items - Stage 5
+          listOfItemReview$itemReviewNegPtBis <-
+            unique(sort(as.numeric(c(
+              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 PtBis"]] < 0])
+            ))))
+        } else {
+          # Low (<20%) Facility - Stage 4
+          listOfItemReview$itemReviewLowFac <-
+            unique(sort(as.numeric(c(
+              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 Facility"]] < 0.2])
+            ))))
+          # Negative PtBis Items - Stage 4
+          listOfItemReview$itemReviewNegPtBis <-
+            unique(sort(as.numeric(c(
+              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 PtBis"]] < 0])
+            ))))
+
+        }
+        # Convert to strings
+        listOfItemReview$itemReviewLowFac <-
+          toString(listOfItemReview$itemReviewLowFac, sep = ",")
+        listOfItemReview$itemReviewNegPtBis <-
+          toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+      }
     }
     return(listOfItemReview)
   }
