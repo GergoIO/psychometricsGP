9c0c7767878e5b0b5afc4eaddb17f03099e79fc4
Gergo <gergopinter@live.co.uk>
2023-01-19 13:30:05 +0000

    author and catches added

--- R/fnDocument.R
+++ R/fnDocument.R
@@ -2,7 +2,12 @@
 #'
 #' @description Use this function to run the document command when updates are made in the PyschometricsGP package. The required packages (devtools, roxygen2) are both loaded and then the document() command is run.
 #'
+#' @author Gergo Pinter, [gergo.pinter@plymouth.ac.uk](mailto:gergo.pinter@plymouth.ac.uk), <gergo.io>
+#'
 #' @return Nothing is returned, a message alerts the user when documenting is complete
+#' @use_package devtools
+#' @use_package roxygen2
+
 #' @export
 #'
 #' @examples fnReload()
@@ -10,6 +15,13 @@
 ################################################################################
 #'
 fnDocument <- function() {
+  if (!require("devtools")) {
+    stop("devtools is not installed. Please install devtools to use this function.")
+  }
+  if (!require("roxygen2")) {
+    stop("roxygen2 is not installed. Please install roxygen2 to use this function.")
+  }
+
   # Load the two packages needed for documenting
   library(devtools)
   library(roxygen2)
