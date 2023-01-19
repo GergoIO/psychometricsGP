cc80396b8573c4d12d53830fd4f2bdd60bd8b3db
Gergo <gergopinter@live.co.uk>
2023-01-19 13:19:04 +0000

    added error catchers etc

--- R/fnReload.R
+++ R/fnReload.R
@@ -10,15 +10,37 @@
 ################################################################################
 #'
 fnReload <- function() {
-  # Detach the package first
-  detach("package:psychometricsGP", unload = TRUE)
-  # Reload the package from the online repo
-  devtools::install_github("GergoIO/psychometricsGP",
-                           # Hidden variable from the secrets file
-                           auth_token = .githubToken,
-                           quiet = TRUE)
-  # Start using the custom package
-  library(psychometricsGP)
+  if(!require(psychometricsGP)) {
+    tryCatch({
+      # Reload the package from the online repo
+      devtools::install_github("GergoIO/psychometricsGP",
+                               auth_token = .githubToken,
+                               quiet = TRUE)
+      # Start using the custom package
+      library(psychometricsGP)
 
-  message("fnReload: Reloading complete")
+      message("fnReload: Reloading complete")
+    }, warning = function(w) {
+      message("fnReload: Warning - ", w$message)
+    }, error = function(e) {
+      message("fnReload: Error - ", e$message)
+    })
+  } else {
+    message("fnReload: psychometricsGP is already loaded")
+  }
 }
+
+# OLD
+# fnReload <- function() {
+#   # Detach the package first
+#   detach("package:psychometricsGP", unload = TRUE)
+#   # Reload the package from the online repo
+#   devtools::install_github("GergoIO/psychometricsGP",
+#                            # Hidden variable from the secrets file
+#                            auth_token = .githubToken,
+#                            quiet = TRUE)
+#   # Start using the custom package
+#   library(psychometricsGP)
+#
+#   message("fnReload: Reloading complete")
+# }
