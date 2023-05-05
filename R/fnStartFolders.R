5a4abeec8b31ba06fcf5ae3902946947cfe416fe
Gergo <gergopinter@live.co.uk>
2023-05-05 11:10:25 +0100

    major rewrite. new checks and methods. old version commented below
    verNum changed to versionNumber

--- R/fnStartFolders.R
+++ R/fnStartFolders.R
@@ -1,7 +1,7 @@
-#' Run functions to create and set variables for save folders
+#' Create Output, Pulse and Images folders.
 #'
 #' @param workingDirectory Define the working directory, where save folders will be created
-#' @param verNum Set a version number - appended to save folders and file names
+#' @param versionNumber Set a version number - appended to save folders and file names
 #' @param createOutputFolder (TRUE/FALSE/NULL) Create an outputs folder in the working directory (with appended version number) if TRUE
 #' @param createPulseFolder (TRUE/FALSE/NULL) Create a folder called Pulse in the Output folder if TRUE
 #' @param createImagesFolder (TRUE/FALSE/NULL) Create an Images folder in the Output folder if TRUE
@@ -9,78 +9,174 @@
 #' @return A list of the paths to the created folders (for Outputs, Images and Pulse Files) is returned.
 #' @export
 #'
-#' @examples listOfPaths <- fnStartFolders(workingDirectory = getwd(), verNum = "v1", createOutputFolder = TRUE, createPulseSubfolder = FALSE, createImagesFolder = TRUE)
+#' @examples listOfPaths <- fnStartFolders(workingDirectory = lFiles$pathWorkingDir #or getwd(), versionNumber = "v1", createOutputFolder = TRUE, createPulseSubfolder = FALSE, createImagesFolder = TRUE)
 
 ################################################################################
 
 # NOTE: The working directory is no longer determined here. Instead it is set in the original file along with the manually configured parameters.
 
+# This function creates the output, pulse and images folders under a specified working directory
+# and version number. If the directory already exists, it will not create it again.
+# Returns a list of paths to the created directories.
 fnStartFolders <- function(workingDirectory = NULL,
-                           verNum = NULL,
-                           createOutputFolder = NULL,
-                           createPulseSubfolder = NULL,
-                           createImagesFolder = NULL) {
+                            versionNumber = NULL,
+                            createOutputFolder = TRUE,
+                            createPulseSubfolder = TRUE,
+                            createImagesFolder = TRUE) {
+  # Check that the working directory and version number are not NULL
   if (is.null(workingDirectory)) {
     stop(
-      "fnStartFolders: Please specify the variable 'workingDirectory' to set the working directory."
+      "fnStartFolders: The 'workingDirectory' variable must be specified to set the working directory."
     )
-  } else if (is.null(verNum)) {
+  }
+  if (is.null(versionNumber)) {
+    stop("fnStartFolders: The 'versionNumber' variable must be specified to name files.")
+  }
+
+  # Check that createOutputFolder, createPulseSubfolder, and createImagesFolder are all booleans
+  if (!is.logical(createOutputFolder) ||
+      !is.logical(createPulseSubfolder) ||
+      !is.logical(createImagesFolder)) {
     stop(
-      "fnStartFolders: Please specify the variable 'verNum' - a version number for file naming."
-    )
-  } else if (is.null(createOutputFolder) |
-             is.null(createImagesFolder) |
-             is.null(createPulseSubfolder)) {
-    warning(
-      "fnStartFolders: Please specify the booleans createOutputFolder, createImagesFolder and createPulseSubfolder to determine which folders should be created. By default, folders will be created for any undefined booleans."
+      "fnStartFolders: The 'createOutputFolder', 'createPulseSubfolder', and 'create_images_folder' variables must be booleans."
     )
   }
 
-  listOfPaths <- list()
+  # Create the output, pulse, and images folders if they do not exist
+  # and createOutputFolder, createPulseSubfolder, and createImagesFolder are TRUE
 
-  # Create folders, if they don't exist
-  # https://stackoverflow.com/questions/40714190/use-apply-functions-dir-exist-and-dir-create
-  if (createOutputFolder != FALSE) {
-    lapply(c(glue('Output ({verNum})')), function(x)
-      if (!dir.exists(x))
-        dir.create(
-          x,
-          showWarnings = TRUE,
-          recursive = FALSE,
-          mode = "0777"
-        ))
-    listOfPaths$pathOutput <-
-      file.path(glue('{workingDirectory}\\Output ({verNum})'))
+  #   __________________________________________________________________________
+  #   Output Folder                                                         ####
+  if (createOutputFolder &&
+      !dir.exists(file.path(workingDirectory,
+                            paste0("Output (", versionNumber, ")")))) {
+    dir.create(file.path(workingDirectory,
+                         paste0("Output (", versionNumber, ")")),
+               recursive = TRUE)
+
+  }
+  #   __________________________________________________________________________
+  #   Pulse Folder                                                          ####
+  if (createPulseSubfolder &&
+      !dir.exists(file.path(
+        workingDirectory,
+        paste0("Output (", versionNumber, ")"),
+        "Pulse"
+      ))) {
+    dir.create(file.path(
+      workingDirectory,
+      paste0("Output (", versionNumber, ")"),
+      "Pulse"
+    ),
+    recursive = TRUE)
+  }
+  #   __________________________________________________________________________
+  #   Images Folder                                                         ####
+  if (createImagesFolder &&
+      !dir.exists(file.path(
+        workingDirectory,
+        paste0("Output (", versionNumber, ")"),
+        "Images"
+      ))) {
+    dir.create(file.path(
+      workingDirectory,
+      paste0("Output (", versionNumber, ")"),
+      "Images"
+    ),
+    recursive = TRUE)
   }
 
-  if (createImagesFolder != FALSE) {
-    lapply(c(glue('Output ({verNum})\\Images')), function(x)
-      if (!dir.exists(x))
-        dir.create(
-          x,
-          showWarnings = TRUE,
-          recursive = FALSE,
-          mode = "0777"
-        ))
-    listOfPaths$pathImages <-
-      file.path(glue('{workingDirectory}\\Output ({verNum})\\Images'))
-  }
+  #   __________________________________________________________________________
+  #   Return Folder Paths                                                   ####
 
-  if (createPulseSubfolder != FALSE) {
-    lapply(c(glue('Output ({verNum})\\Pulse')), function(x)
-      if (!dir.exists(x))
-        dir.create(
-          x,
-          showWarnings = TRUE,
-          recursive = FALSE,
-          mode = "0777"
-        ))
-    listOfPaths$pathOutputPulse <-
-      file.path(glue('{workingDirectory}\\Output ({verNum})\\Pulse'))
-  }
-  # Return the list of paths at the end
+  # Save the file paths to a list
+  listOfPaths <- setNames(
+    list(
+      file.path(workingDirectory, paste0("Output (", versionNumber, ")")),
+      file.path(
+        workingDirectory,
+        paste0("Output (", versionNumber, ")"),
+        "Pulse"
+      ),
+      file.path(
+        workingDirectory,
+        paste0("Output (", versionNumber, ")"),
+        "Images"
+      )
+    ),
+    c("pathOutput", "pathPulse", "pathImages")
+  )
+
+  # Return a list of paths to the created directories
   return(listOfPaths)
-} # END
+}
+
+# fnStartFolders <- function(workingDirectory = NULL,
+#                            verNum = NULL,
+#                            createOutputFolder = NULL,
+#                            createPulseSubfolder = NULL,
+#                            createImagesFolder = NULL) {
+#   if (is.null(workingDirectory)) {
+#     stop(
+#       "fnStartFolders: Please specify the variable 'workingDirectory' to set the working directory."
+#     )
+#   } else if (is.null(verNum)) {
+#     stop(
+#       "fnStartFolders: Please specify the variable 'verNum' - a version number for file naming."
+#     )
+#   } else if (is.null(createOutputFolder) |
+#              is.null(createImagesFolder) |
+#              is.null(createPulseSubfolder)) {
+#     warning(
+#       "fnStartFolders: Please specify the booleans createOutputFolder, createImagesFolder and createPulseSubfolder to determine which folders should be created. By default, folders will be created for any undefined booleans."
+#     )
+#   }
+#
+#   listOfPaths <- list()
+#
+#   # Create folders, if they don't exist
+#   # https://stackoverflow.com/questions/40714190/use-apply-functions-dir-exist-and-dir-create
+#   if (createOutputFolder != FALSE) {
+#     lapply(c(glue('Output ({verNum})')), function(x)
+#       if (!dir.exists(x))
+#         dir.create(
+#           x,
+#           showWarnings = TRUE,
+#           recursive = FALSE,
+#           mode = "0777"
+#         ))
+#     listOfPaths$pathOutput <-
+#       file.path(glue('{workingDirectory}\\Output ({verNum})'))
+#   }
+#
+#   if (createImagesFolder != FALSE) {
+#     lapply(c(glue('Output ({verNum})\\Images')), function(x)
+#       if (!dir.exists(x))
+#         dir.create(
+#           x,
+#           showWarnings = TRUE,
+#           recursive = FALSE,
+#           mode = "0777"
+#         ))
+#     listOfPaths$pathImages <-
+#       file.path(glue('{workingDirectory}\\Output ({verNum})\\Images'))
+#   }
+#
+#   if (createPulseSubfolder != FALSE) {
+#     lapply(c(glue('Output ({verNum})\\Pulse')), function(x)
+#       if (!dir.exists(x))
+#         dir.create(
+#           x,
+#           showWarnings = TRUE,
+#           recursive = FALSE,
+#           mode = "0777"
+#         ))
+#     listOfPaths$pathOutputPulse <-
+#       file.path(glue('{workingDirectory}\\Output ({verNum})\\Pulse'))
+#   }
+#   # Return the list of paths at the end
+#   return(listOfPaths)
+# } # END
 
 # Old method below
 
