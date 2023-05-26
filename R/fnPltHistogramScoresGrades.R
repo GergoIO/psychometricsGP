e35563f866dbacac2db479a14818b8016f7fc31d
Gergo <gergopinter@live.co.uk>
2023-05-26 13:09:51 +0100

    set pass boundary text in a nicer way

--- R/fnPltHistogramScoresGrades.R
+++ R/fnPltHistogramScoresGrades.R
@@ -14,24 +14,32 @@
 ################################################################################
 #'
 fnPltHistogramScoresGradeFreq <- function(data = NULL,
-                                          colScore = NULL,
-                                          colGrade = NULL,
-                                          scoreMax = NULL,
-                                          scorePass = NULL) {
-  #   ________________________________________________________________________
-  #   Check all required variables are defined                            ####
-
-  if (is.null(data) |
-      is.null(colScore) |
-      is.null(colGrade) |
-      is.null(scoreMax) |
+                                           colScore = NULL,
+                                           colGrade = NULL,
+                                           scoreMax = NULL,
+                                           scorePass = NULL) {
+  if (is.null(data) ||
+      is.null(colScore) ||
+      is.null(colGrade) ||
+      is.null(scoreMax) ||
       is.null(scorePass))
   {
     stop("One of the required variables for this function has not been specified.")
   } else {
+    # Set the desired binwidth
+    binwidth <- 1
+
+    # Calc height of max frequency bar - for setting height of pass boundary text
+    max_frequency <-
+      max(hist(
+        data[[colScore]],
+        breaks = seq(min(data[[colScore]]), max(data[[colScore]]) + binwidth, binwidth),
+        plot = FALSE
+      )$counts)
+
     plot <-
       ggplot(data, aes(x = .data[[colScore]], fill = .data[[colGrade]])) +
-      geom_histogram(colour = "black", binwidth = 1) +
+      geom_histogram(colour = "black", binwidth = binwidth) +
       xlab("Score") +
       ylab("Frequency") +
       scale_fill_manual(values = fnGPColours("PF"), guide = guide_legend(reverse = TRUE)) +
@@ -40,18 +48,19 @@ fnPltHistogramScoresGradeFreq <- function(data = NULL,
       annotate(
         geom = "text",
         x = ceiling(scorePass) + 0.1,
-        y = mean(data[[colScore]]),
+        y = max_frequency * 0.65,
         label = "Pass Boundary",
         angle = 90,
         hjust = 0,
         fontface = 2
       ) +
-      scale_y_continuous(expand = expansion(add = c(0, 1))) +
+      scale_y_continuous(expand = expansion(add = c(0, 1)),
+                         limits = c(0, max_frequency + 5)) +
       scale_x_continuous(limits = c(-1, scoreMax + 1),
                          expand = expansion(add = c(-1, -0.4))) +
       theme(aspect.ratio = 2) +
       themeGP()
-    # Return the plot
+
     return(plot)
   }
 }
