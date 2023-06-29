df60c2c12585859a57d6ab5534fb958ec506b7d4
Gergo <gergopinter@live.co.uk>
2023-06-29 13:04:04 +0100

    changed vars to lower snake case

--- R/fnRoundTable.R
+++ R/fnRoundTable.R
@@ -6,13 +6,13 @@
 #' @param data A data frame to be rounded.
 #' @param rows (Vector) Row indexes or row names to specify the rows for rounding.
 #' @param cols (Vector) Column names to specify the columns for rounding.
-#' @param row_decimals (Integer) The number of decimals for row rounding.
-#' @param col_decimals (Integer) The number of decimals for column rounding.
+#' @param rowDecimals (Integer) The number of decimals for row rounding.
+#' @param colDecimals (Integer) The number of decimals for column rounding.
 #' @param rows2 (Vector) Row indexes or row names for a second round of row rounding.
 #' @param cols2 (Vector) Column names for a second round of column rounding.
-#' @param row_decimals2 (Integer) The number of decimals for the second round of row rounding.
-#' @param col_decimals2 (Integer) The number of decimals for the second round of column rounding.
-#' @param rows_first (Boolean) Indicating whether row rounding should be performed before column rounding.
+#' @param rowDecimals2 (Integer) The number of decimals for the second round of row rounding.
+#' @param colDecimals2 (Integer) The number of decimals for the second round of column rounding.
+#' @param rowsFirst (Boolean) Indicating whether row rounding should be performed before column rounding.
 #'
 #' @examples
 #' df <- data.frame(
@@ -25,26 +25,26 @@
 #'   )
 #'
 #' # Example 1: Round specific rows (by row index) with specified decimals
-#' rounded_rows_by_index <- fnRoundTable(df, rows = c(1, 3), row_decimals = 1)
+#' rounded_rows_by_index <- fnRoundTable(df, rows = c(1, 3), rowDecimals = 1)
 #'
 #' # Example 2: Round specific rows (by row name) with specified decimals
-#' rounded_rows_by_name <- fnRoundTable(df, rows = c("Row1", "Row3"), row_decimals = 2)
+#' rounded_rows_by_name <- fnRoundTable(df, rows = c("Row1", "Row3"), rowDecimals = 2)
 #'
 #' # Example 3: Round specific columns (by col index) with specified decimals
-#' rounded_cols_by_index <- fnRoundTable(df, cols = c(2, 3), col_decimals = 1)
+#' rounded_cols_by_index <- fnRoundTable(df, cols = c(2, 3), colDecimals = 1)
 #'
 #' # Example 4: Round specific columns (by col name) with specified decimals
-#' rounded_cols_by_index <- fnRoundTable(df, cols = c("Score1", "Score3"), col_decimals = 2)
+#' rounded_cols_by_index <- fnRoundTable(df, cols = c("Score1", "Score3"), colDecimals = 2)
 #'
 #' # Example 5: Perform row rounding before column rounding (DEFAULT)
-#' rounded_rows_first <- fnRoundTable(df, rows = c(1), cols = c("Score1", "Score3"), row_decimals = 1, col_decimals = 4)
+#' rounded_rowsFirst <- fnRoundTable(df, rows = c(1), cols = c("Score1", "Score3"), rowDecimals = 1, colDecimals = 4)
 #'
 #' # Example 6: Perform col rounding before row rounding
-#' rounded_cols_first <- fnRoundTable(df, rows = c(1), cols = c("Score1", "Score3"), row_decimals = 3, col_decimals = 1, rows_first = FALSE)
+#' rounded_cols_first <- fnRoundTable(df, rows = c(1), cols = c("Score1", "Score3"), rowDecimals = 3, colDecimals = 1, rowsFirst = FALSE)
 #'
 #' # Example 7: Perform a second round of row and column rounding
-#' rounded_second_round <- fnRoundTable(df, rows = c(1), cols = c("Score1", "Score3"), row_decimals = 1, col_decimals = 2,
-#'                                    rows2 = c(3), cols2 = c("Score2"), row_decimals2 = 2, col_decimals2 = 1)
+#' rounded_second_round <- fnRoundTable(df, rows = c(1), cols = c("Score1", "Score3"), rowDecimals = 1, colDecimals = 2,
+#'                                    rows2 = c(3), cols2 = c("Score2"), rowDecimals2 = 2, colDecimals2 = 1)
 #'
 #' @return A modified data frame with rounded values. The cells are coerced to strings
 #' @export
@@ -56,49 +56,49 @@
 fnRoundTable <- function(data,
                          rows = NULL,
                          cols = NULL,
-                         row_decimals = NULL,
-                         col_decimals = NULL,
+                         rowDecimals = NULL,
+                         colDecimals = NULL,
                          rows2 = NULL,
                          cols2 = NULL,
-                         row_decimals2 = NULL,
-                         col_decimals2 = NULL,
-                         rows_first = TRUE) {
+                         rowDecimals2 = NULL,
+                         colDecimals2 = NULL,
+                         rowsFirst = TRUE) {
   # Check if both rows and cols are NULL
   if (is.null(rows) && is.null(cols)) {
     stop("At least one of 'rows' or 'cols' must be provided.")
   }
 
-  # If rows is defined then row_decimals must be defined (+ vice versa and extend for other vars)
-  if (!is.null(rows) && is.null(row_decimals)) {
-    stop("Argument 'row_decimals' must be provided when 'rows' is specified.")
+  # If rows is defined then rowDecimals must be defined (+ vice versa and extend for other vars)
+  if (!is.null(rows) && is.null(rowDecimals)) {
+    stop("Argument 'rowDecimals' must be provided when 'rows' is specified.")
   }
 
-  if (is.null(rows) && !is.null(row_decimals)) {
-    stop("Argument 'rows' must be provided when 'row_decimals' is specified.")
+  if (is.null(rows) && !is.null(rowDecimals)) {
+    stop("Argument 'rows' must be provided when 'rowDecimals' is specified.")
   }
 
-  if (!is.null(cols) && is.null(col_decimals)) {
-    stop("Argument 'col_decimals' must be provided when 'cols' is specified.")
+  if (!is.null(cols) && is.null(colDecimals)) {
+    stop("Argument 'colDecimals' must be provided when 'cols' is specified.")
   }
 
-  if (is.null(cols) && !is.null(col_decimals)) {
-    stop("Argument 'cols' must be provided when 'col_decimals' is specified.")
+  if (is.null(cols) && !is.null(colDecimals)) {
+    stop("Argument 'cols' must be provided when 'colDecimals' is specified.")
   }
 
-  if (!is.null(rows2) && is.null(row_decimals2)) {
-    stop("Argument 'row_decimals2' must be provided when 'rows2' is specified.")
+  if (!is.null(rows2) && is.null(rowDecimals2)) {
+    stop("Argument 'rowDecimals2' must be provided when 'rows2' is specified.")
   }
 
-  if (is.null(rows2) && !is.null(row_decimals2)) {
-    stop("Argument 'rows2' must be provided when 'row_decimals2' is specified.")
+  if (is.null(rows2) && !is.null(rowDecimals2)) {
+    stop("Argument 'rows2' must be provided when 'rowDecimals2' is specified.")
   }
 
-  if (!is.null(cols2) && is.null(col_decimals2)) {
-    stop("Argument 'col_decimals2' must be provided when 'cols2' is specified.")
+  if (!is.null(cols2) && is.null(colDecimals2)) {
+    stop("Argument 'colDecimals2' must be provided when 'cols2' is specified.")
   }
 
-  if (is.null(cols2) && !is.null(col_decimals2)) {
-    stop("Argument 'cols2' must be provided when 'col_decimals2' is specified.")
+  if (is.null(cols2) && !is.null(colDecimals2)) {
+    stop("Argument 'cols2' must be provided when 'colDecimals2' is specified.")
   }
 
   # Get the row names from the data frame
@@ -126,7 +126,7 @@ fnRoundTable <- function(data,
 
   # Define Row Rounding Function
   round_rows <- function(data, rows, decimals) {
-    # Set chars that go into sprintf function later (based on row_decimals)
+    # Set chars that go into sprintf function later (based on rowDecimals)
     rows_format <- paste("%.", decimals, "f", sep = "")
 
     data <- data %>%
@@ -167,54 +167,54 @@ fnRoundTable <- function(data,
       mutate(across(where(is.numeric), as.character))
   }
 
-  # Perform rounding based on rows_first parameter
-  if (rows_first) {
+  # Perform rounding based on rowsFirst parameter
+  if (rowsFirst) {
     # Perform row rounding if rows are specified
-    if (!is.null(rows) && !is.null(row_decimals)) {
-      data <- round_rows(data, rows, row_decimals)
+    if (!is.null(rows) && !is.null(rowDecimals)) {
+      data <- round_rows(data, rows, rowDecimals)
     }
 
     # Perform column rounding if columns are specified
-    if (!is.null(cols) && !is.null(col_decimals)) {
-      data <- round_cols(data, cols, col_decimals)
+    if (!is.null(cols) && !is.null(colDecimals)) {
+      data <- round_cols(data, cols, colDecimals)
     }
   } else {
     # Perform column rounding if columns are specified
-    if (!is.null(cols) && !is.null(col_decimals)) {
-      data <- round_cols(data, cols, col_decimals)
+    if (!is.null(cols) && !is.null(colDecimals)) {
+      data <- round_cols(data, cols, colDecimals)
     }
 
     # Perform row rounding if rows are specified
-    if (!is.null(rows) && !is.null(row_decimals)) {
-      data <- round_rows(data, rows, row_decimals)
+    if (!is.null(rows) && !is.null(rowDecimals)) {
+      data <- round_rows(data, rows, rowDecimals)
     }
   }
 
   # Check if a second round of rounding should be performed
   if (!is.null(rows2) ||
       !is.null(cols2) ||
-      !is.null(row_decimals2) ||
-      !is.null(col_decimals2)) {
-    # Perform second round of rounding based on rows_first parameter
-    if (rows_first) {
+      !is.null(rowDecimals2) ||
+      !is.null(colDecimals2)) {
+    # Perform second round of rounding based on rowsFirst parameter
+    if (rowsFirst) {
       # Perform row rounding if rows are specified
-      if (!is.null(rows2) && !is.null(row_decimals2)) {
-        data <- round_rows(data, rows2, row_decimals2)
+      if (!is.null(rows2) && !is.null(rowDecimals2)) {
+        data <- round_rows(data, rows2, rowDecimals2)
       }
 
       # Perform column rounding if columns are specified
-      if (!is.null(cols2) && !is.null(col_decimals2)) {
-        data <- round_cols(data, cols2, col_decimals2)
+      if (!is.null(cols2) && !is.null(colDecimals2)) {
+        data <- round_cols(data, cols2, colDecimals2)
       }
     } else {
       # Perform column rounding if columns are specified
-      if (!is.null(cols2) && !is.null(col_decimals2)) {
-        data <- round_cols(data, cols2, col_decimals2)
+      if (!is.null(cols2) && !is.null(colDecimals2)) {
+        data <- round_cols(data, cols2, colDecimals2)
       }
 
       # Perform row rounding if rows are specified
-      if (!is.null(rows2) && !is.null(row_decimals2)) {
-        data <- round_rows(data, rows2, row_decimals2)
+      if (!is.null(rows2) && !is.null(rowDecimals2)) {
+        data <- round_rows(data, rows2, rowDecimals2)
       }
     }
   }
