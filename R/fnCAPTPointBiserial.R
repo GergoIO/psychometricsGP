047813acf2178e89257eadc57797c62898bb3252
Gergo Pinter <gergo.pinter@plymouth.ac.uk>
2023-09-15 15:04:07 +0100

    new fn to perform pt biserial calcs

---
+++ R/fnCAPTPointBiserial.R
@@ -0,0 +1,114 @@
+#' Calculate PtBis for CAPT Items
+#'
+#' @param data (dataframe) Incoming long data eg a dataframe with cols StudentID, QuestionNumber and IsCorrect
+#' @param colStudentID (default = "StudentID"), String The name of the column containing the Student ID number
+#' @param colQuestionNumber (default = "QuestionNumber"), String. The name of the column containing the question number in each row
+#' @param colIsCorrect (default = "IsCorrect"), String. The name of the column containing the item score in each row
+#'
+#' @return returns a new dataframe storing the point biserial info for all input items
+#' @export
+#'
+#' @examples ***
+#'
+################################################################################
+#'
+fnCAPTPointBiserial <-
+  function(data,
+           colStudentID = "StudentID",
+           colIsCorrect = "IsCorrect",
+           colQuestionNumber = "QuestionNumber") {
+    # Modify the incoming data
+    # Leave cols: StudentID, QuestionNumber, IsCorrect, TotalScore
+    data <-
+      data |>
+      select(all_of(c(
+        colStudentID,
+        colQuestionNumber,
+        colIsCorrect
+      ))) |>
+      # Use group_by_at so that the group_by can have a variable as the col name
+      group_by_at(colStudentID) |>
+      mutate(TotalScore = sum(across(all_of(colIsCorrect))))
+
+    # Create an empty data frame to store the results
+    dataPtBis <- data.frame(
+      Test = character(),
+      QuestionNumber = character(),
+      CorrelationCoefficient = numeric(),
+      PValue = numeric(),
+      stringsAsFactors = FALSE
+    )
+
+    # Initialise some vars to notify of progress
+    nItemsProcessed <- 0
+    pctItemsProgressedNext <- 20
+
+    # Save the unique item numbers present in the input data
+    # These are the items that the below loop will loop over and try to calculate the PtBis
+    listItemNumbers <- unique(data[[colQuestionNumber]])
+
+    # Loop through each question
+    for (itemNumber in listItemNumbers) {
+      # Filter data for the current question for this loop
+      dataItem <-
+        data |>
+        filter(QuestionNumber == itemNumber)
+
+      # Calculate the point-biserial correlation coefficient and p-value for the current item with IsCorrect
+      # PtBis cannot be calculate in some circumstances eg
+      # if no-one got the item correct /
+      # if everyone got the item correct /
+      # if the student only got that question correct /
+      # if there are two or fewer responses for that item
+      if (sum(dataItem[[colIsCorrect]]) == 0 |
+          sum(dataItem[[colIsCorrect]]) == nrow(dataItem) |
+          sum(is.finite(dataItem$TotalScore)) < 2 |
+          nrow(dataItem) == 2) {
+        correlationResult <- (list(r = NA, p = NA))
+      } else {
+        result <- tryCatch(
+          # PtBis actually calculated here:
+          cor.test(dataItem[[colIsCorrect]], dataItem$TotalScore),
+          error = function(e) {
+            cat("Error occurred during cor.test for the following data:\n")
+            print(dataItem)
+            correlationResult <- (NULL)
+          }
+        )
+        # If PtBis is not calculated, add values to the PtBis results instead
+        if (is.null(result)) {
+          correlationResult <- (list(r = NA, p = NA))
+        } else {
+          correlationResult <- (list(r = result$estimate, p = result$p.value))
+        }
+      }
+
+      # Store the result in the result data frame
+      # Add the results from this loop to the overall results
+      dataPtBis <- rbind(
+        dataPtBis,
+        data.frame(
+          Test = cnst$assessment,
+          QuestionNumber = itemNumber,
+          CorrelationCoefficient = correlationResult$r,
+          PValue = correlationResult$p,
+          CorrelationCoefficientSig = ifelse(correlationResult$p < 0.05, correlationResult$r, NA)
+        )
+      )
+
+      # Update progress and display message when reaching the next multiple of 20% progress
+      nItemsProcessed <- nItemsProcessed + 1
+      pctItemsProcessedCurr <-
+        # Get fraction of the total items processed
+        nItemsProcessed / length(unique(lResponses$main$QuestionNumber)) * 100
+      if (pctItemsProcessedCurr >= pctItemsProgressedNext) {
+        print(paste(
+          "PtBis Calculation Progress:",
+          floor(pctItemsProcessedCurr),
+          "% of items considered"
+        ))
+        pctItemsProgressedNext <- pctItemsProgressedNext + 20
+      }
+    }
+    return(dataPtBis)
+  }
