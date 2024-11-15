ce7a1c1be083f0a2602bd42f3553809d88abbbc9
Gergo Pinter <gergopinter@live.co.uk>
2024-11-15 09:22:15 +0000

    redid format and added sig ptbis for AKT, IDS, ADTK

--- R/fnItemReview.R
+++ R/fnItemReview.R
@@ -34,148 +34,303 @@ fnItemReview <- function(assessmentType = NULL,
 
     if (assessmentType %in% c("ADK")) {
       # Low (<20%) Facility
-      listOfItemReview$itemReviewLowFac <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Facility"]] < 0.2])
-        ))))
-      listOfItemReview$itemReviewLowFac <-
-        toString(listOfItemReview$itemReviewLowFac, sep = ",")
+      listOfItemReview <- list(
+        itemReviewLowFac = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Facility"]] < 0.2]))
+        ))),
 
-      # Negative Growth Items
-      listOfItemReview$itemReviewNegGrowth <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 vs 2 Growth"]] < 0])
-        ))))
-      listOfItemReview$itemReviewNegGrowth <-
-        toString(listOfItemReview$itemReviewNegGrowth, sep = ",")
+        # Negative Growth Items
+        itemReviewNegGrowth = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 vs 2 Growth"]] < 0]))
+        ))),
 
-      # Negative PtBis Items
-      listOfItemReview$itemReviewNegPtBis <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 PtBis"]] < 0])
-        ))))
-      listOfItemReview$itemReviewNegPtBis <-
-        toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+        # Negative PtBis Items
+        itemReviewNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 PtBis"]] < 0]))
+        ))),
+
+        # Statistically Significant Negative PtBis Items
+        itemReviewSigNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Sig PtBis"]] < 0]))
+        )))
+      )
     } else if (assessmentType %in% c("ADTK")) {
-      #   ______________________________________________________________________
-      #   ADTK                                                              ####
-
       # Low (<20%) Facility
-      listOfItemReview$itemReviewLowFac <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 Facility"]] < 0.2])
-        ))))
-      listOfItemReview$itemReviewLowFac <-
-        toString(listOfItemReview$itemReviewLowFac, sep = ",")
+      listOfItemReview <- list(
+        itemReviewLowFac = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 Facility"]] < 0.2]))
+        ))),
 
-      # Negative Growth Items
-      listOfItemReview$itemReviewNegGrowth <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 vs 2 Growth"]] < 0])
-        ))))
-      listOfItemReview$itemReviewNegGrowth <-
-        toString(listOfItemReview$itemReviewNegGrowth, sep = ",")
+        # Negative Growth Items
+        itemReviewNegGrowth = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 vs 2 Growth"]] < 0]))
+        ))),
 
-      # Negative PtBis Items
-      listOfItemReview$itemReviewNegPtBis <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 PtBis"]] < 0])
-        ))))
-      listOfItemReview$itemReviewNegPtBis <-
-        toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+        # Negative PtBis Items
+        itemReviewNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 PtBis"]] < 0]))
+        ))),
+
+        # Statistically Significant Negative PtBis Items
+        itemReviewSigNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 Sig PtBis"]] < 0]))
+        )))
+      )
     } else if (assessmentType %in% c("AMK")) {
-      #   ______________________________________________________________________
-      #   AMK                                                               ####
-
+      # AMK logic
       if (is.null(testInYear)) {
         stop("For AMK assessments, the test in year (testInYear) variable must be defined.")
       } else {
+        # Check if testInYear is 1 or 2
         if (testInYear %in% c(1, 2)) {
           # Low (<20%) Facility - Stage 5
-          listOfItemReview$itemReviewLowFac <-
-            unique(sort(as.numeric(c(
-              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Facility"]] < 0.2])
-            ))))
           # Negative PtBis Items - Stage 5
-          listOfItemReview$itemReviewNegPtBis <-
-            unique(sort(as.numeric(c(
-              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 PtBis"]] < 0])
-            ))))
+          listOfItemReview <- list(
+            itemReviewLowFac = toString(unique(sort(
+              as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Facility"]] < 0.2]))
+            ))),
+            itemReviewNegPtBis = toString(unique(sort(
+              as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 PtBis"]] < 0]))
+            )))
+          )
         } else {
           # Low (<20%) Facility - Stage 4
-          listOfItemReview$itemReviewLowFac <-
-            unique(sort(as.numeric(c(
-              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 Facility"]] < 0.2])
-            ))))
           # Negative PtBis Items - Stage 4
-          listOfItemReview$itemReviewNegPtBis <-
-            unique(sort(as.numeric(c(
-              na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 PtBis"]] < 0])
-            ))))
-
+          listOfItemReview <- list(
+            itemReviewLowFac = toString(unique(sort(
+              as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 Facility"]] < 0.2]))
+            ))),
+            itemReviewNegPtBis = toString(unique(sort(
+              as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 PtBis"]] < 0]))
+            )))
+          )
         }
-        # Convert to strings
-        listOfItemReview$itemReviewLowFac <-
-          toString(listOfItemReview$itemReviewLowFac, sep = ",")
-        listOfItemReview$itemReviewNegPtBis <-
-          toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
       }
     } else if (assessmentType %in% c("PAPT")) {
-      #   ______________________________________________________________________
-      #   PAPT                                                              ####
-
       # Low (<20%) Facility
-      listOfItemReview$itemReviewLowFac <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 Facility"]] < 0.2])
-        ))))
-      listOfItemReview$itemReviewLowFac <-
-        toString(listOfItemReview$itemReviewLowFac, sep = ",")
+      listOfItemReview <- list(
+        itemReviewLowFac = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 Facility"]] < 0.2]))
+        ))),
 
-      # Negative Growth Items
-      listOfItemReview$itemReviewNegGrowth <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 vs 1 Growth"]] < 0])
-        ))))
-      listOfItemReview$itemReviewNegGrowth <-
-        toString(listOfItemReview$itemReviewNegGrowth, sep = ",")
+        # Negative Growth Items
+        itemReviewNegGrowth = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 vs 1 Growth"]] < 0]))
+        ))),
 
-      # Negative PtBis Items
-      listOfItemReview$itemReviewNegPtBis <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 PtBis"]] < 0])
-        ))))
-      listOfItemReview$itemReviewNegPtBis <-
-        toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+        # Negative PtBis Items
+        itemReviewNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 PtBis"]] < 0]))
+        ))),
 
-      # Statistically Significant Negative PtBis Items
-      listOfItemReview$itemReviewSigNegPtBis <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 Sig PtBis"]] < 0])
-        ))))
-      listOfItemReview$itemReviewSigNegPtBis <-
-        toString(listOfItemReview$itemReviewSigNegPtBis, sep = ",")
-
+        # Statistically Significant Negative PtBis Items
+        itemReviewSigNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 Sig PtBis"]] < 0]))
+        )))
+      )
     } else if (assessmentType %in% c("IDS", "Y1KT", "AKT")) {
-      #   ______________________________________________________________________
-      #   IDS/Y1KT                                                          ####
-
       # Low (<20%) Facility
-      listOfItemReview$itemReviewLowFac <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["Facility"]] < 0.2])
-        ))))
-      listOfItemReview$itemReviewLowFac <-
-        toString(listOfItemReview$itemReviewLowFac, sep = ",")
+      listOfItemReview <- list(
+        itemReviewLowFac = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Facility"]] < 0.2]))
+        ))),
 
-      # Negative PtBis Items
-      listOfItemReview$itemReviewNegPtBis <-
-        unique(sort(as.numeric(c(
-          na.omit(itemAnalysisData$Item[itemAnalysisData[["PtBis"]] < 0])
-        ))))
-      listOfItemReview$itemReviewNegPtBis <-
-        toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+        # Negative PtBis Items
+        itemReviewNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["PtBis"]] < 0]))
+        ))),
+
+        # Statistically Significant Negative PtBis Items
+        itemReviewSigNegPtBis = toString(unique(sort(
+          as.numeric(na.omit(itemAnalysisData$Item[itemAnalysisData[["Sig PtBis"]] < 0]))
+        )))
+      )
     }
     return(listOfItemReview)
+
   }
 }
+
+# if (is.null(assessmentType) |
+#     is.null(itemAnalysisData)) {
+#   stop("One of the required variables for this function has not been specified.")
+# } else{
+#   listOfItemReview <- list()
+#   #   ________________________________________________________________________
+#   #   ADK                                                                 ####
+#
+#   if (assessmentType %in% c("ADK")) {
+#     # Low (<20%) Facility
+#     listOfItemReview$itemReviewLowFac <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Facility"]] < 0.2])
+#       ))))
+#     listOfItemReview$itemReviewLowFac <-
+#       toString(listOfItemReview$itemReviewLowFac, sep = ",")
+#
+#     # Negative Growth Items
+#     listOfItemReview$itemReviewNegGrowth <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 vs 2 Growth"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewNegGrowth <-
+#       toString(listOfItemReview$itemReviewNegGrowth, sep = ",")
+#
+#     # Negative PtBis Items
+#     listOfItemReview$itemReviewNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewNegPtBis <-
+#       toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+#
+#     # Statistically Significant Negative PtBis Items
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Sig PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       toString(listOfItemReview$itemReviewSigNegPtBis, sep = ",")
+#
+#
+#   } else if (assessmentType %in% c("ADTK")) {
+#     #   ______________________________________________________________________
+#     #   ADTK                                                              ####
+#
+#     # Low (<20%) Facility
+#     listOfItemReview$itemReviewLowFac <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 Facility"]] < 0.2])
+#       ))))
+#     listOfItemReview$itemReviewLowFac <-
+#       toString(listOfItemReview$itemReviewLowFac, sep = ",")
+#
+#     # Negative Growth Items
+#     listOfItemReview$itemReviewNegGrowth <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 vs 2 Growth"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewNegGrowth <-
+#       toString(listOfItemReview$itemReviewNegGrowth, sep = ",")
+#
+#     # Negative PtBis Items
+#     listOfItemReview$itemReviewNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewNegPtBis <-
+#       toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+#
+#     # Statistically Significant Negative PtBis Items
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 3 Sig PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       toString(listOfItemReview$itemReviewSigNegPtBis, sep = ",")
+#
+#
+#   } else if (assessmentType %in% c("AMK")) {
+#     #   ______________________________________________________________________
+#     #   AMK                                                               ####
+#
+#     if (is.null(testInYear)) {
+#       stop("For AMK assessments, the test in year (testInYear) variable must be defined.")
+#     } else {
+#       if (testInYear %in% c(1, 2)) {
+#         # Low (<20%) Facility - Stage 5
+#         listOfItemReview$itemReviewLowFac <-
+#           unique(sort(as.numeric(c(
+#             na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 Facility"]] < 0.2])
+#           ))))
+#         # Negative PtBis Items - Stage 5
+#         listOfItemReview$itemReviewNegPtBis <-
+#           unique(sort(as.numeric(c(
+#             na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 5 PtBis"]] < 0])
+#           ))))
+#       } else {
+#         # Low (<20%) Facility - Stage 4
+#         listOfItemReview$itemReviewLowFac <-
+#           unique(sort(as.numeric(c(
+#             na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 Facility"]] < 0.2])
+#           ))))
+#         # Negative PtBis Items - Stage 4
+#         listOfItemReview$itemReviewNegPtBis <-
+#           unique(sort(as.numeric(c(
+#             na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 4 PtBis"]] < 0])
+#           ))))
+#       }
+#       # Convert to strings
+#       listOfItemReview$itemReviewLowFac <-
+#         toString(listOfItemReview$itemReviewLowFac, sep = ",")
+#       listOfItemReview$itemReviewNegPtBis <-
+#         toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+#     }
+#   } else if (assessmentType %in% c("PAPT")) {
+#     #   ______________________________________________________________________
+#     #   PAPT                                                              ####
+#
+#     # Low (<20%) Facility
+#     listOfItemReview$itemReviewLowFac <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 Facility"]] < 0.2])
+#       ))))
+#     listOfItemReview$itemReviewLowFac <-
+#       toString(listOfItemReview$itemReviewLowFac, sep = ",")
+#
+#     # Negative Growth Items
+#     listOfItemReview$itemReviewNegGrowth <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 vs 1 Growth"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewNegGrowth <-
+#       toString(listOfItemReview$itemReviewNegGrowth, sep = ",")
+#
+#     # Negative PtBis Items
+#     listOfItemReview$itemReviewNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewNegPtBis <-
+#       toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+#
+#     # Statistically Significant Negative PtBis Items
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Stage 2 Sig PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       toString(listOfItemReview$itemReviewSigNegPtBis, sep = ",")
+#
+#   } else if (assessmentType %in% c("IDS", "Y1KT", "AKT")) {
+#     #   ______________________________________________________________________
+#     #   IDS/Y1KT                                                          ####
+#
+#     # Low (<20%) Facility
+#     listOfItemReview$itemReviewLowFac <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Facility"]] < 0.2])
+#       ))))
+#     listOfItemReview$itemReviewLowFac <-
+#       toString(listOfItemReview$itemReviewLowFac, sep = ",")
+#
+#     # Negative PtBis Items
+#     listOfItemReview$itemReviewNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewNegPtBis <-
+#       toString(listOfItemReview$itemReviewNegPtBis, sep = ",")
+#
+#     # Statistically Significant Negative PtBis Items
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       unique(sort(as.numeric(c(
+#         na.omit(itemAnalysisData$Item[itemAnalysisData[["Sig PtBis"]] < 0])
+#       ))))
+#     listOfItemReview$itemReviewSigNegPtBis <-
+#       toString(listOfItemReview$itemReviewSigNegPtBis, sep = ",")
+#
+#   }
+#   return(listOfItemReview)
+# }
+# }
