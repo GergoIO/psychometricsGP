e5b6ad5c35f6b7ff95d83e63783dcf6a0cd51d1e
Gergo Pinter <gergopinter@live.co.uk>
2025-02-10 10:22:02 +0000

    change vars to snake case

--- R/fn_point_biserial_significant.R
+++ R/fn_point_biserial_significant.R
@@ -1,185 +1,105 @@
 #' Calculate PtBis and Statistical Significance for Test Items
 #'
-#' @param data (dataframe) Incoming long data eg a dataframe with cols StudentID, ItemID and IsCorrect
+#' @param data (dataframe) Incoming long data eg a dataframe with cols student_id, item_id, and is_correct
 #' @param assessment (String) The name of the test.
-#' @param colStudentID (default = "StudentID"), String The name of the column containing the Student ID number
-#' @param colIsCorrect (default = "IsCorrect"), String. The name of the column containing the item score in each row
-#' @param colItemID (default = "ItemID"), String. The name of the column containing the ItemID in each row
-#' @param significanceLevel (default = 0.05) Numeric. Set the significance level for each point biserial correlation
+#' @param col_student_id (default = "StudentID"), String The name of the column containing the Student ID number
+#' @param col_is_correct (default = "IsCorrect"), String. The name of the column containing the item score in each row
+#' @param col_item_id (default = "ItemID"), String. The name of the column containing the ItemID in each row
+#' @param significance_level (default = 0.05) Numeric. Set the significance level for each point biserial correlation
 #'
 #' @return returns a new dataframe storing the point biserial info for all input items
 #' @export
 #'
 #' @examples
-#'  df <- data.frame(
-# StudentID = c(
-#   "001",
-#   "001",
-#   "001",
-#   "001",
-#   "001",
-#   "002",
-#   "002",
-#   "002",
-#   "002",
-#   "002",
-#   "003",
-#   "003",
-#   "003",
-#   "003",
-#   "003",
-#   "004",
-#   "004",
-#   "004",
-#   "004",
-#   "004"
-# ),
-# ItemID = c(
-#   "B",
-#   "C",
-#   "D",
-#   "E",
-#   "F",
-#   "B",
-#   "C",
-#   "D",
-#   "E",
-#   "F",
-#   "A",
-#   "B",
-#   "D",
-#   "E",
-#   "F",
-#   "B",
-#   "C",
-#   "D",
-#   "E",
-#   "F"
-# ),
-# IsCorrect = c(1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0)
-# )
-#
-# df |> fn_point_biserial_significant()
-#'
-################################################################################
-#'
-fn_point_biserial_significant <-
-  function(data,
-           assessment = cnst$assessment,
-           colStudentID = "StudentID",
-           colIsCorrect = "IsCorrect",
-           colItemID = "ItemID",
-           significanceLevel = 0.05) {
-    # Modify the incoming data
-    # Leave cols: StudentID, ItemID, IsCorrect, TotalScore
-    data <-
-      data |>
-      dplyr::select(all_of(c(colStudentID,
-                      colItemID,
-                      colIsCorrect))) |>
-      # Use group_by_at so that the group_by can have a variable as the col name
-      group_by_at(colStudentID) |>
-      mutate(TotalScore = sum(across(all_of(colIsCorrect))))
+#' df <- data.frame(
+#'   StudentID = rep(1:3, each = 6),
+#'   ItemID = rep(c("A", "B", "C", "D", "E", "F"), 3),
+#'   IsCorrect = c(1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0)
+#' )
+#' df |> fn_point_biserial_significant()
 
-    # Create an empty data frame to store the results
-    dataPtBis <- data.frame(
-      Test = character(),
-      ItemID = character(),
-      CorrelationCoefficient_cor = numeric(),
-      PValue_cor = numeric(),
-      CorrelationCoefficient_cor_test = numeric(),
-      PValue_cor_test = numeric(),
-      stringsAsFactors = FALSE
-    )
+fn_point_biserial_significant <- function(data,
+                                          assessment = "Test",
+                                          col_student_id = "StudentID",
+                                          col_is_correct = "IsCorrect",
+                                          col_item_id = "ItemID",
+                                          significance_level = 0.05) {
+  data <- data |>
+    select(all_of(c(
+      col_student_id, col_item_id, col_is_correct
+    ))) |>
+    group_by_at(col_student_id) |>
+    mutate(total_score = sum(across(all_of(col_is_correct))))
 
-    # Initialise some vars to notify of progress
-    nItemsProcessed <- 0
-    pctItemsProgressedNext <- 20
+  data_pt_bis <- data.frame(
+    assessment = character(),
+    item_id = character(),
+    correlation_coefficient_cor = numeric(),
+    p_value_cor = numeric(),
+    correlation_coefficient_cor_test = numeric(),
+    p_value_cor_test = numeric(),
+    stringsAsFactors = FALSE
+  )
 
-    # Save the unique item numbers present in the input data
-    # These are the items that the below loop will loop over and try to calculate the PtBis
-    listItemNumbers <- unique(data[[colItemID]])
+  n_items_processed <- 0
+  pct_items_progressed_next <- 20
+  list_item_numbers <- unique(data[[col_item_id]])
 
-    # Loop through each item
-    for (itemNumber in listItemNumbers) {
-      # Filter data for the current item for this loop
-      dataItem <-
-        data |>
-        filter(!!sym(colItemID) == itemNumber)
+  for (item_number in list_item_numbers) {
+    data_item <- data |> filter(!!sym(col_item_id) == item_number)
+    correlation_cor <- cor(data_item[[col_is_correct]], data_item$total_score - data_item[[col_is_correct]])
 
-      # Calculate the correlation coefficient using cor()
-      correlation_cor <-
-        cor(dataItem[[colIsCorrect]], dataItem$TotalScore - dataItem[[colIsCorrect]])
+    if (sum(data_item[[col_is_correct]]) == 0 |
+        sum(data_item[[col_is_correct]]) == nrow(data_item) |
+        sum(is.finite(data_item$total_score)) < 2 |
+        nrow(data_item) == 2) {
+      correlation_result <- list(r = NA, p = NA)
+    } else {
+      total_score_excl_curr_item <- data_item$total_score - data_item[[col_is_correct]]
 
-      # Calculate the point-biserial correlation coefficient and p-value for the current item with IsCorrect
-      # PtBis cannot be calculated in some circumstances eg
-      # if no-one got the item correct /
-      # if everyone got the item correct /
-      # if the student only got that item correct /
-      # if there are two or fewer responses for that item
-      if (sum(dataItem[[colIsCorrect]]) == 0 |
-          sum(dataItem[[colIsCorrect]]) == nrow(dataItem) |
-          sum(is.finite(dataItem$TotalScore)) < 2 |
-          nrow(dataItem) == 2) {
-        correlationResult <- (list(r = NA, p = NA))
-      } else {
-        # Calculate the total score excluding the current item for each student
-        totalScore_excl_curr_item <-
-          dataItem$TotalScore - dataItem[[colIsCorrect]]
-
-        result <- tryCatch(
-          # PtBis actually calculated here:
-          cor.test(dataItem[[colIsCorrect]], totalScore_excl_curr_item),
-          error = function(e) {
-            cat("Error occurred during cor.test for the following data:\n")
-            print(dataItem)
-            correlationResult <- (NULL)
-          }
-        )
-        # If PtBis is not calculated, add values to the PtBis results instead
-        if (is.null(result)) {
-          correlationResult <- (list(r = NA, p = NA))
-        } else {
-          correlationResult <- (list(r = result$estimate, p = result$p.value))
+      result <- tryCatch(
+        cor.test(data_item[[col_is_correct]], total_score_excl_curr_item),
+        error = function(e) {
+          cat("Error occurred during cor.test for the following data:\n")
+          print(data_item)
+          correlation_result <- list(r = NA, p = NA)
         }
-      }
-
-      # Store the result in the result data frame
-      # Add the results from this loop to the overall results
-      dataPtBis <- rbind(
-        dataPtBis,
-        data.frame(
-          Assessment = assessment,
-          ItemID = itemNumber,
-          CorrelationCoefficient_cor = correlation_cor,
-          PValue_cor = NA,
-          # NA for correlation coefficient from cor()
-          CorrelationCoefficient_cor_test = correlationResult$r,
-          PValue_cor_test = correlationResult$p,
-          CorrelationCoefficientSignificant = ifelse(
-            correlationResult$p < significanceLevel,
-            correlationResult$r,
-            NA
-          )
-        )
       )
 
-      # Update progress and display message when reaching the next multiple of 20% progress
-      nItemsProcessed <- nItemsProcessed + 1
-      pctItemsProcessedCurr <-
-        # Get fraction of the total items processed
-        nItemsProcessed / length(unique(listItemNumbers)) * 100
-      if (pctItemsProcessedCurr >= pctItemsProgressedNext) {
-        print(paste(
-          "PtBis Calculation Progress:",
-          floor(pctItemsProcessedCurr),
-          "% of items considered"
-        ))
-        pctItemsProgressedNext <- pctItemsProgressedNext + 20
+      if (is.null(result)) {
+        correlation_result <- list(r = NA, p = NA)
+      } else {
+        correlation_result <- list(r = result$estimate, p = result$p.value)
       }
     }
-    # dataPtBis |>
-    #   as_tibble() |>
-    #   mutate_at(vars(ItemID), as.numeric)
-    return(dataPtBis)
+
+    data_pt_bis <- rbind(
+      data_pt_bis,
+      data.frame(
+        Assessment = assessment,
+        item_id = item_number,
+        correlation_coefficient_cor = correlation_cor,
+        p_value_cor = NA,
+        correlation_coefficient_cor_test = correlation_result$r,
+        p_value_cor_test = correlation_result$p,
+        correlation_coefficient_significant = ifelse(
+          correlation_result$p < significance_level,
+          correlation_result$r,
+          NA
+        )
+      )
+    )
+
+    n_items_processed <- n_items_processed + 1
+    pct_items_processed_curr <- n_items_processed / length(unique(list_item_numbers)) * 100
+    if (pct_items_processed_curr >= pct_items_progressed_next) {
+      print(paste(
+        "PtBis Calculation Progress:",
+        floor(pct_items_processed_curr),
+        "% of items considered"
+      ))
+      pct_items_progressed_next <- pct_items_progressed_next + 20
+    }
   }
+  return(data_pt_bis)
+}
